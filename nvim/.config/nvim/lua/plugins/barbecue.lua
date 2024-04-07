52d04de010c0c91f430992f81ac4f593a2e54264
metziger23 <metziger23@gmail.com>
2024-04-07 13:49:50 +0300

    added bufferline for tabs, removed show modifed from barbecue and also removed greeter

--- nvim/.config/nvim/lua/plugins/barbecue.lua
+++ nvim/.config/nvim/lua/plugins/barbecue.lua
@@ -6,8 +6,5 @@ return {
 		"SmiteshP/nvim-navic",
 		"nvim-tree/nvim-web-devicons", -- optional dependency
 	},
-	opts = {
-    show_modified = true
-		-- configurations go here
-	},
+  config = true
 }
