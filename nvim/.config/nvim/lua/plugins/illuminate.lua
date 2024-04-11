961eead8ea65fe58a66171e7439c27de5cb5f023
metziger23 <metziger23@gmail.com>
2024-04-11 23:54:12 +0300

    added filetypes_denylist to illuminate

--- nvim/.config/nvim/lua/plugins/illuminate.lua
+++ nvim/.config/nvim/lua/plugins/illuminate.lua
@@ -7,6 +7,31 @@ return {
 		local illuminate = require("illuminate")
 		illuminate.configure({
 			delay = 250,
+      filetypes_denylist = {
+        "mason",
+        "harpoon",
+        "DressingInput",
+        "NeogitCommitMessage",
+        "qf",
+        "dirvish",
+        "oil",
+        "minifiles",
+        "fugitive",
+        "alpha",
+        "NvimTree",
+        "lazy",
+        "NeogitStatus",
+        "Trouble",
+        "netrw",
+        "lir",
+        "DiffviewFiles",
+        "Outline",
+        "Jaq",
+        "spectre_panel",
+        "toggleterm",
+        "DressingSelect",
+        "TelescopePrompt",
+      },
 		})
 
 	end,
