d46c4d9432f30e77fecf6b2178a8f8115f25447c
metziger23 <metziger23@gmail.com>
2024-04-14 12:32:57 +0300

    not wide completion menu but combined with lspkind

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -12,19 +12,40 @@ return {
 		"hrsh7th/cmp-nvim-lsp-signature-help",
 	},
 	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+		local lspkind = require("lspkind")
+
+		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+		require("luasnip.loaders.from_vscode").lazy_load()
 
     local check_backspace = function()
       local col = vim.fn.col "." - 1
       return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
     end
-		local cmp = require("cmp")
 
-		local luasnip = require("luasnip")
+    local format_fn = function(_, item)
+          item.menu = ""
+          local fixed_width = 40
+          local content = item.abbr
 
-		local lspkind = require("lspkind")
+      if fixed_width then
+        vim.o.pumwidth = fixed_width
+      end
 
-		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-		require("luasnip.loaders.from_vscode").lazy_load()
+      local win_width = vim.api.nvim_win_get_width(0)
+
+      local max_content_width = fixed_width and fixed_width - 10 or math.floor(win_width * 0.2)
+
+      if #content > max_content_width then
+        item.abbr = vim.fn.strcharpart(content, 0, max_content_width - 3) .. "..."
+      else
+        item.abbr = content .. (" "):rep(max_content_width - #content)
+      end
+
+      return item
+    end
+
 
 		-- `/` cmdline setup.
 		cmp.setup.cmdline("/", {
@@ -107,54 +128,14 @@ return {
 				{ name = "path" }, -- file system paths
         { name = 'nvim_lsp_signature_help' }
 			}),
-			-- configure lspkind for vs-code like pictograms in completion menu
-      -- https://github.com/hrsh7th/nvim-cmp/discussions/609#discussioncomment-5727678
+
       formatting = {
-        fields = { "abbr", "menu", "kind" },
-        format = function(entry, item)
-          -- Define menu shorthand for different completion sources.
-          local menu_icon = {
-            nvim_lsp = "NLSP",
-            nvim_lua = "NLUA",
-            luasnip  = "LSNP",
-            buffer   = "BUFF",
-            path     = "PATH",
-          }
-          -- Set the menu "icon" to the shorthand for each completion source.
-          item.menu = menu_icon[entry.source.name]
-
-          -- Set the fixed width of the completion menu to 60 characters.
-          -- fixed_width = 20
-
-          -- Set 'fixed_width' to false if not provided.
-          local fixed_width = fixed_width or false
-
-          -- Get the completion entry text shown in the completion window.
-          local content = item.abbr
-
-          -- Set the fixed completion window width.
-          if fixed_width then
-            vim.o.pumwidth = fixed_width
-          end
-
-          -- Get the width of the current window.
-          local win_width = vim.api.nvim_win_get_width(0)
-
-          -- Set the max content width based on either: 'fixed_width'
-          -- or a percentage of the window width, in this case 20%.
-          -- We subtract 10 from 'fixed_width' to leave room for 'kind' fields.
-          local max_content_width = fixed_width and fixed_width - 10 or math.floor(win_width * 0.2)
-
-          -- Truncate the completion entry text if it's longer than the
-          -- max content width. We subtract 3 from the max content width
-          -- to account for the "..." that will be appended to it.
-          if #content > max_content_width then
-            item.abbr = vim.fn.strcharpart(content, 0, max_content_width - 3) .. "..."
-          else
-            item.abbr = content .. (" "):rep(max_content_width - #content)
-          end
-          return item
-        end,
+        fields = { "abbr", "kind" },
+        format = lspkind.cmp_format({
+          maxwidth = 40,
+          ellipsis_char = "...",
+          before = format_fn,
+        }),
       },
 		})
 	end,
