568fc9310efe20d5fc2fd695435ada27545949d7
metziger23 <metziger23@gmail.com>
2024-04-18 21:40:14 +0300

    add instead of append for harpoon2

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -7,7 +7,7 @@ return
     require("harpoon"):setup()
   end,
   keys = {
-    { "<leader>A", function() require("harpoon"):list():append() end, desc = "Harpoon Add File", },
+    { "<leader>A", function() require("harpoon"):list():add() end, desc = "Harpoon Add File", },
     { "<leader>a", function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "Harpoon Quick Menu", },
     { "<leader>1", function() require("harpoon"):list():select(1) end, desc = "which_key_ignore", },
     { "<leader>2", function() require("harpoon"):list():select(2) end, desc = "which_key_ignore", },
