37a701cac8e726dc2c634c57939d82eecebe5e99
metziger23 <metziger23@gmail.com>
2024-04-21 21:14:24 +0300

    added Neogit and Diffview

---
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,13 @@
+return {
+	"sindrets/diffview.nvim", -- optional - Diff integration
+	config = true,
+	cmd = {
+		"DiffviewClose",
+		"DiffviewLog",
+		"DiffviewOpen",
+		"DiffviewRefresh",
+		"DiffviewFocusFiles",
+		"DiffviewFileHistory",
+		"DiffviewToggleFiles",
+	},
+}
