fbc73575237d5af8a3e00e74f03ef129b7cd02f5
metziger23 <metziger23@gmail.com>
2024-04-21 21:31:55 +0300

    changed some mappings in yanky

--- nvim/.config/nvim/lua/plugins/yanky.lua
+++ nvim/.config/nvim/lua/plugins/yanky.lua
@@ -12,7 +12,7 @@ return {
   },
   keys = {
     { "<leader>y", function() require("telescope").extensions.yank_history.yank_history({ }) end, desc = "Open Yank History" },
-    { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank text" },
+    -- { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank text" },
     { "p", "<Plug>(YankyPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after cursor" },
     { "P", "<Plug>(YankyPutBefore)", mode = { "n", "x" }, desc = "Put yanked text before cursor" },
     { "gp", "<Plug>(YankyGPutAfter)", mode = { "n", "x" }, desc = "Put yanked text after selection" },
@@ -27,7 +27,7 @@ return {
     { "<p", "<Plug>(YankyPutIndentAfterShiftLeft)", desc = "Put and indent left" },
     { ">P", "<Plug>(YankyPutIndentBeforeShiftRight)", desc = "Put before and indent right" },
     { "<P", "<Plug>(YankyPutIndentBeforeShiftLeft)", desc = "Put before and indent left" },
-    { "=p", "<Plug>(YankyPutAfterFilter)", desc = "Put after applying a filter" },
-    { "=P", "<Plug>(YankyPutBeforeFilter)", desc = "Put before applying a filter" },
+    { "<leader>p", "<Plug>(YankyPutAfterFilter)", desc = "Put after applying a filter" },
+    { "<leader>P", "<Plug>(YankyPutBeforeFilter)", desc = "Put before applying a filter" },
   },
 }
