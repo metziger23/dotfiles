bb82fdbde940a2a573827a84056461fa85721e9f
metziger23 <metziger23@gmail.com>
2024-04-28 16:16:27 +0300

    better configured yanky and substitute

--- nvim/.config/nvim/lua/plugins/substitute.lua
+++ nvim/.config/nvim/lua/plugins/substitute.lua
@@ -1,14 +1,17 @@
 return {
 	"gbprod/substitute.nvim",
-	opts = {
-    highlight_substituted_text = {
-      enabled = false,
-    },
-	},
 	keys = {
 		{ "s", function() require("substitute").operator() end, mode = "n", desc = "Substitute" },
 		{ "ss", function() require("substitute").line() end, mode = "n", desc = "Substitute Line" },
 		{ "S", function() require("substitute").eol() end, mode = "n", desc = "Substitute EOL" },
 		{ "s", function() require("substitute").visual() end, mode = "x", desc = "Substitute Visual" },
 	},
+  config = function ()
+    require("substitute").setup({
+      highlight_substituted_text = {
+        enabled = false,
+      },
+      on_substitute = require("yanky.integration").substitute(),
+    })
+  end
 }
