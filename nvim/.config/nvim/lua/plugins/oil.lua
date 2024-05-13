636631a395705455571932331e50c1c5a784c11a
metziger23 <metziger23@gmail.com>
2024-05-13 18:55:26 +0300

    added winbar for oil to show cur dir

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -13,6 +13,9 @@ return {
 				"size",
 				"mtime",
 			},
+      win_options = {
+        winbar = "%{v:lua.require('oil').get_current_dir()}",
+      },
 		})
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 	end,
