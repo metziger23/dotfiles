d3820c0279bea152c247631e3452493c18b68581
metziger23 <metziger23@gmail.com>
2024-05-17 23:10:53 +0300

    separate config for lazygit to work with nvim-remote

--- nvim/.config/nvim/lua/plugins/lazygit.lua
+++ nvim/.config/nvim/lua/plugins/lazygit.lua
@@ -9,5 +9,7 @@ return {
 	},
 	config = function()
 		require("telescope").load_extension("lazygit")
+    vim.g.lazygit_use_custom_config_file_path = 1
+    vim.g.lazygit_config_file_path = vim.fn.expand("$HOME/.config/lazygit/lazygit-nvim-remote-config.yml")
 	end,
 }
