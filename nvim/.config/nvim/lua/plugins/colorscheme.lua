3163b21de8fc132236d2a7b629d3cbf4f711db77
metziger23 <metziger23@gmail.com>
2024-05-20 18:58:47 +0300

    disabled catppuccin integration with illuminate

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -7,6 +7,12 @@ return {
 		config = function()
 			require("catppuccin").setup({
 				floating_border = "on",
+				integrations = {
+					illuminate = {
+						enabled = false,
+						lsp = false,
+					},
+				},
 			})
 			vim.cmd.colorscheme("catppuccin")
 		end,
