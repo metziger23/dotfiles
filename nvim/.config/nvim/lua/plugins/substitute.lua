c331a836ddb99ed7b3991f96dacef3feb0a2bf74
metziger23 <metziger23@gmail.com>
2024-06-03 23:21:06 +0300

    added substitute reindent and exchange

--- nvim/.config/nvim/lua/plugins/substitute.lua
+++ nvim/.config/nvim/lua/plugins/substitute.lua
@@ -5,6 +5,16 @@ return {
 		{ "ss", function() require("substitute").line() end, mode = "n", desc = "Substitute Line" },
 		{ "S", function() require("substitute").eol() end, mode = "n", desc = "Substitute EOL" },
 		{ "s", function() require("substitute").visual() end, mode = "x", desc = "Substitute Visual" },
+
+    { "<leader>s", function() require("substitute").operator({ modifiers = { "reindent" } }) end, mode = "n", desc = "Substitute (reindent)" },
+    { "<leader>ss", function() require("substitute").line({ modifiers = { "reindent" } }) end, mode = "n", desc = "Substitute Line (reindent)" },
+    { "<leader>S", function() require("substitute").eol({ modifiers = { "reindent" } }) end, mode = "n", desc = "Substitute EOL (reindent)" },
+    { "<leader>s", function() require("substitute").visual({ modifiers = { "reindent" } }) end, mode = "x", desc = "Substitute Visual (reindent)" },
+
+    {"sx", function() require("substitute.exchange").operator() end, mode = "n", desc = "Substitute Exchange" },
+    {"sxx", function() require("substitute.exchange").line() end, mode = "n", desc = "Substitute Exchange Line" },
+    {"X", function() require("substitute.exchange").visual() end, mode = "x", desc = "Substitute Exchange Visual" },
+    {"sxc", function() require("substitute.exchange").cancel() end, mode = "n", desc = "Substitute Exchange Cancel" },
 	},
   config = function ()
     require("substitute").setup({
