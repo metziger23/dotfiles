1ec51784dc1deb23da439d7cdb404b7d8e5ab5aa
metziger23 <metziger23@gmail.com>
2024-07-14 22:23:34 +0300

    Meta as Alt for switching tabs

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -5,3 +5,8 @@ c.url.default_page = 'https://www.google.com/'
 c.url.searchengines = {
     'DEFAULT':  'https://google.com/search?hl=en&q={}',
 }
+for i in range (1, 10):
+    meta = '<Meta-' + str(i) + '>'
+    alt = '<Alt-' + str(i) + '>'
+    c.bindings.key_mappings[meta] = alt
+
