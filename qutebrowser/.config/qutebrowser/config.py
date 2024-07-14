70d312880dabc6673462538b6f83c9564148947e
metziger23 <metziger23@gmail.com>
2024-07-14 18:29:21 +0300

    use google.com as a start page and default page

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,4 +1,7 @@
 config.load_autoconfig()
+
+c.url.start_pages = ['https://www.google.com/']
+c.url.default_page = 'https://www.google.com/'
 c.url.searchengines = {
     'DEFAULT':  'https://google.com/search?hl=en&q={}',
 }
