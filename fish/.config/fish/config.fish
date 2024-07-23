558f5041cd979d8bf6abc006f0779fc4ce369c1b
metziger23 <metziger23@gmail.com>
2024-07-23 21:29:45 +0300

    added brew path, fzf, zoxide to fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+  if test -d /opt/homebrew
+    # Homebrew is installed on MacOS
+    /opt/homebrew/bin/brew shellenv | source
+  end
+
+  fzf --fish | source # Set up fzf key bindings
+  zoxide init fish | source
 end
