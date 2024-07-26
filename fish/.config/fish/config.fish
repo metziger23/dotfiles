19d1c30371e78eb3bd4d5b9b7dcc43a565a8f00d
metziger23 <metziger23@gmail.com>
2024-07-26 20:49:27 +0300

    Squash merge fish-config into master

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,20 @@
+if status is-interactive
+  set -g fish_greeting
+  set -U fish_prompt_pwd_dir_length 0
+    # Commands to run in interactive sessions can go here
+  if test -d /opt/homebrew
+    # Homebrew is installed on MacOS
+    /opt/homebrew/bin/brew shellenv | source
+  end
+
+  abbr -a lg lazygit
+  abbr -a n nvim
+
+  set -x BAT_THEME "Catppuccin Mocha"
+  set -x EDITOR nvim
+  set -x VISUAL nvim
+  set -x MANPAGER "nvim +Man!"
+
+  fzf --fish | source # Set up fzf key bindings
+  zoxide init fish | source
+end
