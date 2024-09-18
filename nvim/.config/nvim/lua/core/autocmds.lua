ce71ca9b75c23d840b8491976fdb9cc68a3b0a0c
mikhail <mikhail@localhost.localdomain>
2024-09-18 23:57:01 +0300

    added DiagnosticToggle autocmd

--- nvim/.config/nvim/lua/core/autocmds.lua
+++ nvim/.config/nvim/lua/core/autocmds.lua
@@ -26,3 +26,12 @@ vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
   end,
 })
 
+vim.api.nvim_create_user_command("DiagnosticToggle", function()
+  local config = vim.diagnostic.config
+  local vt = config().virtual_text
+  config {
+    virtual_text = not vt,
+    underline = not vt,
+    signs = not vt,
+  }
+end, { desc = "toggle diagnostic" })
