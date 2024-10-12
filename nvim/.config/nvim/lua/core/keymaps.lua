23cab0b8e20344dcbb98fbf0636a70cf25c0ae96
metziger23 <metziger23@gmail.com>
2024-10-12 21:56:51 +0300

    Added keymap for :nohl

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -2,3 +2,5 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<PageUp>", "<C-u>", { desc = "Scroll half page up" })
 vim.keymap.set("n", "<PageDown>", "<C-d>", { desc = "Scroll half page down" })
+
+vim.keymap.set("n", "<C-_>", ":nohl<CR>", { desc = "Clear search highlights", silent = true })
