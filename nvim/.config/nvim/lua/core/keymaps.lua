234565f1355f5cab7449f35a5b2017935c152452
metziger23 <metziger23@gmail.com>
2024-10-13 18:30:17 +0300

    Removed keymaps for PageUp, PageDown and changed for :nohl

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -1,6 +1,3 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<PageUp>", "<C-u>", { desc = "Scroll half page up" })
-vim.keymap.set("n", "<PageDown>", "<C-d>", { desc = "Scroll half page down" })
-
-vim.keymap.set("n", "<C-_>", ":nohl<CR>", { desc = "Clear search highlights", silent = true })
+vim.keymap.set("n", "<leader>'", ":nohl<CR>", { desc = "Clear search highlights", silent = true })
