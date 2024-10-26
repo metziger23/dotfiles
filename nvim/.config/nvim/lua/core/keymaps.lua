196e1b8e8a0613dc400e3a0f5db8527e5a69b638
metziger23 <metziger23@gmail.com>
2024-10-26 18:51:32 +0300

    added keymaps for managing tabs

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -1,3 +1,7 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("t", [[<C-/>]], [[<C-\><C-n>]])
+
+vim.keymap.set("n", "<leader>tn", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+vim.keymap.set("n", "<leader>tc", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
