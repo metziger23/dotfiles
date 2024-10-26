ecbbd92215fe5dc9055b7e1c3738ba6981239445
metziger23 <metziger23@gmail.com>
2024-10-26 15:09:05 +0300

    round border for Diffview help popup

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -1,6 +1,5 @@
 return {
 	"sindrets/diffview.nvim", -- optional - Diff integration
-	config = true,
   dependencies = { "nvim-tree/nvim-web-devicons" },
 	cmd = {
 		"DiffviewClose",
@@ -11,4 +10,15 @@ return {
 		"DiffviewFileHistory",
 		"DiffviewToggleFiles",
 	},
+  config = function ()
+    require("diffview").setup({})
+    vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+      pattern = "diffview:///panels/*",
+      callback = function()
+        if vim.api.nvim_win_get_config(0).zindex then
+          vim.api.nvim_win_set_config(0, { border = "rounded" })
+        end
+      end,
+    })
+  end
 }
