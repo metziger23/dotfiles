c6a22b4d52424009ee15bd6ccf09276c2472cb53
metziger23 <metziger23@gmail.com>
2024-12-09 00:29:52 +0300

    removed stay-centered

--- nvim/.config/nvim/lua/plugins/stay-centered.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "arnamak/stay-centered.nvim",
-  opts = function()
-    require("stay-centered").setup({
-      -- Add any configurations here, like skip_filetypes if needed
-      -- skip_filetypes = {"lua", "typescript"},
-    })
-  end,
-}
