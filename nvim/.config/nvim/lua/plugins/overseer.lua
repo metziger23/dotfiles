70b663854128001eac236d56889d44851dad33ca
metziger23 <metziger23@gmail.com>
2025-01-02 16:46:51 +0300

    Added OverseerRestartLast autocmd

--- nvim/.config/nvim/lua/plugins/overseer.lua
+++ nvim/.config/nvim/lua/plugins/overseer.lua
@@ -21,6 +21,17 @@ return {
 				end,
 			},
 		})
+
+    vim.api.nvim_create_user_command("OverseerRestartLast", function()
+      local overseer = require("overseer")
+      local tasks = overseer.list_tasks({ recent_first = true })
+      if vim.tbl_isempty(tasks) then
+        vim.notify("No tasks found", vim.log.levels.WARN)
+      else
+        overseer.run_action(tasks[1], "restart")
+      end
+    end, {})
+    vim.keymap.set("n", "<A-l>", "<cmd>OverseerRestartLast<CR>", { desc = "Overseer Restart Last Command"})
 	end,
 	keys = {
 		{ "<leader>oo", "<cmd>OverseerToggle<CR>", mode = "n", desc = "Overseer Toggle" },
