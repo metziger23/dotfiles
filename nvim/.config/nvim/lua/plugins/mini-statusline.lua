70c3ba21009ec744dd8f52e753db047a7bc0f6d6
metziger23 <metziger23@gmail.com>
2025-01-03 23:10:57 +0300

    global customized mini-statusline with cwd printing

--- nvim/.config/nvim/lua/plugins/mini-statusline.lua
+++ nvim/.config/nvim/lua/plugins/mini-statusline.lua
@@ -1,5 +1,37 @@
+local function get_cwd()
+  return vim.fn.fnamemodify(vim.fn.getcwd(), ":~")
+end
+
 return {
   "echasnovski/mini.statusline",
   version = "*",
-  config = true,
+  config = function()
+    local MiniStatusline = require("mini.statusline")
+    MiniStatusline.setup({
+      content = {
+        -- Content for active window
+        active = function()
+          local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+          local git = MiniStatusline.section_git({ trunc_width = 40 })
+          local diff = MiniStatusline.section_diff({ trunc_width = 75 })
+          local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+          local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
+          local filename = MiniStatusline.section_filename({ trunc_width = 140 })
+          local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+          local location = MiniStatusline.section_location({ trunc_width = 75 })
+          local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
+
+          return MiniStatusline.combine_groups({
+            { hl = mode_hl, strings = { mode } },
+            { hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics, lsp } },
+            "%<", -- Mark general truncate point
+            { hl = "MiniStatuslineFilename", strings = { get_cwd() } },
+            "%=", -- End left alignment
+            { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+            { hl = mode_hl, strings = { search, location } },
+          })
+        end,
+      },
+    })
+  end,
 }
