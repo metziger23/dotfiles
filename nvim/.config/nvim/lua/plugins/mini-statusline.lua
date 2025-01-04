e7adb4705a23e7bfe9a6bea8cc3977841a56757c
metziger23 <metziger23@gmail.com>
2025-01-04 13:49:35 +0300

    refactored code in mini-statusline

--- nvim/.config/nvim/lua/plugins/mini-statusline.lua
+++ nvim/.config/nvim/lua/plugins/mini-statusline.lua
@@ -1,27 +1,26 @@
-local symbols = {
-	["CANCELED"] = " ",
-	["FAILURE"] = "󰅚 ",
-	["SUCCESS"] = "󰄴 ",
-	["RUNNING"] = "󰑮 ",
+local statuses = {
+	["CANCELED"] = { symbol = " ", hl = "markdownH3" },
+	["FAILURE"] = { symbol = "󰅚 ", hl = "markdownH1" },
+	["SUCCESS"] = { symbol = "󰄴 ", hl = "markdownH4" },
+	["RUNNING"] = { symbol = "󰑮 ", hl = "markdownH1" },
 }
 
-local function overseer_task_status(status)
+local function get_overseer_task_status(status)
+	local result = { hl = "MiniStatuslineInactive", strings = { nil } }
 	if not package.loaded.overseer then
-		return nil
+		return result
 	end
+
 	local tasks = require("overseer.task_list").list_tasks({ unique = true })
 	local tasks_by_status = require("overseer.util").tbl_group_by(tasks, "status")
 	if not tasks_by_status[status] then
-		return nil
+		return result
 	end
-	return string.format("%s%d", symbols[status], #tasks_by_status[status])
-end
 
-local function overseer_task_status_hl(string, hl)
-  if string == nil then
-    return "MiniStatuslineInactive"
-  end
-  return hl
+	local string = string.format("%s%d", statuses[status].symbol, #tasks_by_status[status])
+	result.hl = statuses[status].hl
+	result.strings = { string }
+	return result
 end
 
 local function get_cwd()
@@ -29,7 +28,7 @@ local function get_cwd()
 end
 
 local function get_relative_filename()
-  return vim.fn.expand("%:.")
+	return vim.fn.expand("%:.")
 end
 
 return {
@@ -46,28 +45,23 @@ return {
 					local diff = MiniStatusline.section_diff({ trunc_width = 75 })
 					local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
 					local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
----@diagnostic disable-next-line: unused-local
+					---@diagnostic disable-next-line: unused-local
 					local filename = MiniStatusline.section_filename({ trunc_width = 140 })
 					local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
 					local location = MiniStatusline.section_location({ trunc_width = 75 })
 					local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
-          local overseer_canceled = overseer_task_status("CANCELED")
-          local overseer_failure = overseer_task_status("FAILURE")
-          local overseer_success = overseer_task_status("SUCCESS")
-          local overseer_running = overseer_task_status("RUNNING")
 
 					return MiniStatusline.combine_groups({
 						{ hl = mode_hl, strings = { mode } },
 						{ hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics, lsp } },
 						"%<", -- Mark general truncate point
-            { hl = overseer_task_status_hl(overseer_canceled, "markdownH3"), strings = { overseer_canceled } },
-            { hl = overseer_task_status_hl(overseer_failure, "markdownH1"), strings = { overseer_failure } },
-            { hl = overseer_task_status_hl(overseer_success, "markdownH4"), strings = { overseer_success } },
-            { hl = overseer_task_status_hl(overseer_running, "markdownH2"), strings = { overseer_running } },
+						get_overseer_task_status("CANCELED"),
+						get_overseer_task_status("FAILURE"),
+						get_overseer_task_status("SUCCESS"),
+						get_overseer_task_status("RUNNING"),
 						{ hl = "MiniStatuslineFilename", strings = { get_relative_filename() } },
-
 						"%=", -- End left alignment
-            { hl = "MiniStatuslineFilename", strings = { get_cwd() } },
+						{ hl = "MiniStatuslineFilename", strings = { get_cwd() } },
 						{ hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
 						{ hl = mode_hl, strings = { search, location } },
 					})
