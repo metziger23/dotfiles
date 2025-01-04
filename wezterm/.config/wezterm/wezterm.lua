86c6eaf1e0403923ed672a49790d64bc7b721c0d
metziger23 <metziger23@gmail.com>
2025-01-05 01:27:23 +0300

    formatting only for wezterm.lua

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -8,20 +8,20 @@ config.initial_rows = 100
 config.initial_cols = 300
 
 -- For example, changing the color scheme:
-config.color_scheme = 'catppuccin-mocha'
+config.color_scheme = "catppuccin-mocha"
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 
 config.font_size = 16.5
 config.adjust_window_size_when_changing_font_size = false
 
-config.default_cursor_style = 'SteadyBar'
+config.default_cursor_style = "SteadyBar"
 config.max_fps = 120
 
 -- Cursor
 -- config.default_cursor_style = "BlinkingBar"
 config.force_reverse_video_cursor = true
 
-config.window_padding = { left = 0, right = 0, top = 0, bottom = 0, }
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 
 -- and finally, return the configuration to wezterm
 return config
