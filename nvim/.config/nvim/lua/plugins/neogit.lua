fd14c8c7fdc58245b3698c9efbdde119215931d4
metziger23 <metziger23@gmail.com>
2025-01-04 14:52:55 +0300

    use kitty graph_style only with kitty

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -1,3 +1,11 @@
+local function get_graph_style()
+  local term = vim.env.TERM
+  if string.match(term, "kitty") then
+    return "kitty"
+  end
+  return "ascii"
+end
+
 return {
 	"NeogitOrg/neogit",
   cmd = { "Neogit"--[[ , "Neogit cwd", "Neogit kind", "Neogit commit" ]]},
@@ -9,6 +17,6 @@ return {
 	keys = {
 		{ "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit" },
 	},
-  opts = { graph_style = "kitty" },
+  opts = { graph_style = get_graph_style() },
   config = true
 }
