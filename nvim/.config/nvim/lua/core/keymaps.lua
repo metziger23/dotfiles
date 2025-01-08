0cc03b244fd4a78187a3c9a49fe1c9373098dabf
metziger23 <metziger23@gmail.com>
2025-01-08 18:58:42 +0300

    add  "<M-s>" keymap to save current file

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -1,7 +1,9 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("t", [[<C-/>]], [[<C-\><C-n>]])
+vim.keymap.set("t", [[<C-/>]], [[<C-\><C-n>]], { desc = "Escape terminal mode" })
 
 vim.keymap.set("n", "<leader>tn", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 vim.keymap.set("n", "<leader>tc", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+vim.keymap.set("n", "<M-s>", "<cmd>w<CR>", { desc = "Save current file" })
