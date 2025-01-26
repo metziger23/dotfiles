a96093b3eeb5746b2df08236fad02edd954b1efd
metziger23 <metziger23@gmail.com>
2025-01-26 22:15:23 +0300

    Added keymaps for rotating windows

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -7,3 +7,8 @@ vim.keymap.set("n", "<leader>tc", "<cmd>tabclose<CR>", { desc = "Close current t
 vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
 vim.keymap.set("n", "<M-s>", "<cmd>w<CR>", { desc = "Save current file" })
+
+vim.keymap.set("n", "<C-w><S-Left>",  "<C-w><S-h>", { desc = "Rotate current window left" })
+vim.keymap.set("n", "<C-w><S-Down>",  "<C-w><S-j>", { desc = "Rotate current window down" })
+vim.keymap.set("n", "<C-w><S-Up>",    "<C-w><S-k>", { desc = "Rotate current window up" })
+vim.keymap.set("n", "<C-w><S-Right>", "<C-w><S-l>", { desc = "Rotate current window right" })
