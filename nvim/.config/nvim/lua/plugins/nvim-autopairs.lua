1ef3f62c35a3dddc738b08f591e7c4bbc50930c2
metziger23 <metziger23@gmail.com>
2025-01-28 01:16:26 +0300

    disabled autopairs for snacks_picker_input

--- nvim/.config/nvim/lua/plugins/nvim-autopairs.lua
+++ nvim/.config/nvim/lua/plugins/nvim-autopairs.lua
@@ -10,6 +10,7 @@ return {
 
     -- configure autopairs
     autopairs.setup({
+      disable_filetype = { "TelescopePrompt", "spectre_panel", "snacks_picker_input" },
       check_ts = true, -- enable treesitter
       ts_config = {
         lua = { "string" }, -- don't add pairs in lua string treesitter nodes
