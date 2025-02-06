9e019a044692f4afc99c7ab22177df687f739f31
metziger23 <metziger23@gmail.com>
2025-02-06 22:58:46 +0300

    added qmlformat config to conform

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,16 +1,31 @@
 return {
-  'stevearc/conform.nvim',
-
-  config = function()
-    local conform = require("conform")
-
-    conform.setup({
-      formatters_by_ft = {
-        lua = { "stylua" },
-        c = { 'clang_format' },
-        cpp = { 'clang_format' },
-      },
-    })
-
-  end
+	"stevearc/conform.nvim",
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				c = { "clang_format" },
+				cpp = { "clang_format" },
+				qml = { "qmlformat" },
+			},
+			formatters = {
+				qmlformat = {
+					meta = {
+						url = "https://doc.qt.io/qt-6//qtqml-tooling-qmlformat.html",
+						description = "qmlformat is a tool that automatically formats QML files in accordance with the QML Coding Conventions.",
+					},
+					command = "qmlformat",
+					args = {
+            "--functions-spacing",
+            "--objects-spacing",
+            "--normalize",
+            "--indent-width",
+            "4",
+            "--force",
+						"$FILENAME",
+					},
+				},
+			},
+		})
+	end,
 }
