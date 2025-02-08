6159710851826458b9f0d7ed62a6249f3a9c7411
metziger23 <metziger23@gmail.com>
2025-02-08 23:16:05 +0300

    Revert "Show buffers instead of tabs for bufferline"
    This reverts commit 13924461224b85e204caba323ee8c803cc84085f.

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,14 +1,9 @@
 return {
-	"akinsho/bufferline.nvim",
-  event = "VeryLazy",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {
-		options = {
-			-- mode = "tabs",
-		},
-	},
-	keys = {
-		{ "<S-l>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
-		{ "<S-h>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
-	},
+  "akinsho/bufferline.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    options = {
+      mode = "tabs",
+    },
+  },
 }
