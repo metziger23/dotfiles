195998193a88f44b1a4327aae6cd7fe1e41d4f4c
metziger23 <metziger23@gmail.com>
2025-02-08 13:17:07 +0300

    Show buffers instead of tabs for bufferline

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,9 +1,14 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    options = {
-      mode = "tabs",
-    },
-  },
+	"akinsho/bufferline.nvim",
+  event = "VeryLazy",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		options = {
+			-- mode = "tabs",
+		},
+	},
+	keys = {
+		{ "<S-l>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+		{ "<S-h>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+	},
 }
