6adb4b75dca1155af03f97175d1deb03af24acfb
metziger23 <metziger23@gmail.com>
2025-02-09 23:48:04 +0300

    fixed lazygit resize issue

--- nvim/.config/nvim/lua/plugins/snacks-lazygit.lua
+++ nvim/.config/nvim/lua/plugins/snacks-lazygit.lua
@@ -1,3 +1,19 @@
+local function toggle_lazygit(open_lazygit_callback, open_lazygit_callback_opts)
+	open_lazygit_callback_opts = open_lazygit_callback_opts or {}
+	local float_term = open_lazygit_callback(open_lazygit_callback_opts)
+	local created_buffer = float_term.buf
+	local opts = { desc = "Toggle Lazygit" }
+
+	vim.api.nvim_buf_set_keymap(created_buffer, "t", "<M-g>", "<cmd>close<CR>", opts)
+
+	vim.api.nvim_create_autocmd({ "VimResized" }, {
+		buffer = created_buffer,
+		callback = function()
+			vim.api.nvim_input([[<C-\><C-n>^i]])
+		end,
+	})
+end
+
 return {
 	"folke/snacks.nvim",
 	opts = {
@@ -11,17 +27,17 @@ return {
 		{
 			"<M-g>",
 			function()
-				local float_term = require("snacks").lazygit()
-        local created_buffer = float_term.buf
-        vim.api.nvim_buf_set_keymap(
-          created_buffer, "t", "<M-g>", "<cmd>close<CR>", { desc = "Toggle Lazygit" })
+        local callback = require("snacks").lazygit.open
+				toggle_lazygit(callback)
 			end,
 			desc = "Lazygit",
 		},
 		{
 			"<M-C-g>",
 			function()
-				require("snacks").lazygit({ cwd = require("snacks").git.get_root() })
+        local callback = require("snacks").lazygit.open
+        local opts = { cwd = require("snacks").git.get_root() }
+        toggle_lazygit(callback, opts)
 			end,
 			desc = "Lazygit current buffer git root",
 		},
