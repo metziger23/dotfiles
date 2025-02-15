57b5e04e15db6c378af8a26a8828f78f7bcbfd9a
metziger23 <metziger23@gmail.com>
2025-02-15 15:33:20 +0300

    added noice

---
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,31 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		presets = {
+			lsp_doc_border = true, -- add a border to hover docs and signature help
+		},
+	},
+	dependencies = {
+		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+		"MunifTanjim/nui.nvim",
+		-- OPTIONAL:
+		--   `nvim-notify` is only needed, if you want to use the notification view.
+		--   If not available, we use `mini` as the fallback
+		"rcarriga/nvim-notify",
+	},
+	config = function(_, opts)
+		require("noice").setup(opts)
+		vim.keymap.set({ "n", "i", "s" }, "<c-f>", function()
+			if not require("noice.lsp").scroll(4) then
+				return "<c-f>"
+			end
+		end, { silent = true, expr = true })
+
+		vim.keymap.set({ "n", "i", "s" }, "<c-b>", function()
+			if not require("noice.lsp").scroll(-4) then
+				return "<c-b>"
+			end
+		end, { silent = true, expr = true })
+	end,
+}
