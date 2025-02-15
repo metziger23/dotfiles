a8c449806efdb8c8d2520ad0c1a492617ae6ba0f
metziger23 <metziger23@gmail.com>
2025-02-15 14:36:01 +0300

    rewritten and refactored lazygit toggleterm cofig

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -1,24 +1,44 @@
+local function setup_terminal(configuration)
+	local term_opts = {
+		direction = "float",
+		float_opts = {
+			border = "curved",
+		},
+	}
+
+	if configuration.term_opts then
+		term_opts = vim.tbl_deep_extend("force", term_opts, configuration.term_opts or {})
+	end
+
+	term_opts.on_open = function(term)
+		local opts = { buffer = term.bufnr, noremap = true, silent = true }
+		opts.desc = configuration.desc
+		vim.keymap.set("t", configuration.keymap, function()
+			term:toggle()
+		end, opts)
+	end
+
+	local term = require("toggleterm.terminal").Terminal:new(term_opts)
+
+	local function toggle()
+		if configuration.toggle_pre_hook then
+			configuration.toggle_pre_hook(term)
+		end
+		term:toggle()
+	end
+
+	local opts = { noremap = true, silent = true }
+	opts.desc = configuration.desc
+	vim.keymap.set("n", configuration.keymap, toggle, opts)
+end
+
 local lazygit_config_path = vim.fn.expand("$HOME/.config/lazygit/lazygit-nvim-remote-config.yml")
 local lazygit_cmd = "lazygit -ucf " .. lazygit_config_path
 
-local main_toggleterm_opts = {
-	direction = "float",
-	float_opts = {
-		border = "curved",
-	},
-}
-
-local lazygit_opts = vim.tbl_deep_extend("force", main_toggleterm_opts, {
+local lazygit_opts = {
 	cmd = lazygit_cmd,
 	dir = "git_dir",
-	on_open = function(term)
-		local opts = { buffer = term.bufnr, noremap = true, silent = true }
-		opts.desc = "Toggle Lazygit"
-		vim.keymap.set("t", "<M-g>", function()
-			term:toggle()
-		end, opts)
-	end,
-})
+}
 
 return {
 	"akinsho/toggleterm.nvim",
@@ -30,45 +50,44 @@ return {
 	},
 	config = function(_, opts)
 		require("toggleterm").setup(opts)
-		local modes = { "n", "t", "x" }
-		local Terminal = require("toggleterm.terminal").Terminal
-		local lazygit = Terminal:new(vim.deepcopy(lazygit_opts))
-		local lazygit_cur_buf = Terminal:new(vim.deepcopy(lazygit_opts))
-		local lazygit_filter_cur_buf = Terminal:new(vim.deepcopy(lazygit_opts))
-		local main_toggleterm = Terminal:new(vim.deepcopy(main_toggleterm_opts))
 
-		vim.keymap.set({ "n", "t", "x" }, "<M-C-t>", function()
-			main_toggleterm:toggle()
-		end, { desc = "Toggle main Toggleterm" })
+		local main_term_config = { keymap = "<M-C-t>", desc = "Toggle main Toggleterm" }
+		setup_terminal(main_term_config)
 
-		local function lazygit_toggle()
-			local dot_git_path = vim.fn.finddir(".git", vim.fn.getcwd() .. ";")
-			lazygit.dir = vim.fn.fnamemodify(dot_git_path, ":h")
-			lazygit:toggle()
-		end
+		local lazygit_config = {
+			keymap = "<M-g>",
+			desc = "Toggle Lazygit",
+			term_opts = lazygit_opts,
+			toggle_pre_hook = function(term)
+				local dot_git_path = vim.fn.finddir(".git", vim.fn.getcwd() .. ";")
+				term.dir = vim.fn.fnamemodify(dot_git_path, ":h")
+			end,
+		}
 
-		vim.keymap.set("n", "<M-g>", lazygit_toggle, { desc = "Toggle lazygit" })
+		setup_terminal(lazygit_config)
 
-		local function lazygit_cur_buf_toggle()
-			-- local dot_git_path = vim.fn.finddir(".git", ".;")
-			lazygit_cur_buf.dir = require("snacks").git.get_root()
-			lazygit_cur_buf:toggle()
-		end
+		local lazygit_cur_buf_config = {
+			keymap = "<M-C-G>",
+			desc = "Toggle Lazygit (current buffer)",
+			term_opts = lazygit_opts,
+			toggle_pre_hook = function(term)
+				term.dir = require("snacks").git.get_root()
+			end,
+		}
 
-		vim.keymap.set("n", "<M-C-G>", lazygit_cur_buf_toggle, { desc = "Toggle lazygit (current buffer)" })
+		setup_terminal(lazygit_cur_buf_config)
 
-		local function lazygit_filter_cur_buf_toggle()
-			lazygit_filter_cur_buf.cmd = lazygit_cmd .. " --filter " .. vim.fn.expand("%:p")
-			local dot_git_path = vim.fn.finddir(".git", ".;")
-			lazygit_filter_cur_buf.dir = vim.fn.fnamemodify(dot_git_path, ":h")
-			lazygit_filter_cur_buf:toggle()
-		end
+		local lazygit_log_cur_buf_config = {
+			keymap = "<M-j>",
+			desc = "Toggle lazygit (log current buffer)",
+			term_opts = lazygit_opts,
+			toggle_pre_hook = function(term)
+				term.cmd = lazygit_cmd .. " --filter " .. vim.fn.expand("%:p")
+				local dot_git_path = vim.fn.finddir(".git", ".;")
+				term.dir = vim.fn.fnamemodify(dot_git_path, ":h")
+			end,
+		}
 
-		vim.keymap.set(
-			"n",
-			"<leader>gl",
-			lazygit_filter_cur_buf_toggle,
-			{ desc = "Toggle lazygit (log current buffer)" }
-		)
+		setup_terminal(lazygit_log_cur_buf_config)
 	end,
 }
