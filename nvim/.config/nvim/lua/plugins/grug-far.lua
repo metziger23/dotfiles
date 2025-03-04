1411221481a005fef7cc73efeae16e3a400a863e
metziger23 <metziger23@gmail.com>
2025-03-04 22:45:11 +0300

    grug-far keybinding for only current buffer

--- nvim/.config/nvim/lua/plugins/grug-far.lua
+++ nvim/.config/nvim/lua/plugins/grug-far.lua
@@ -14,6 +14,14 @@ return {
 		{
 			"<leader>r",
 			function()
+				require("grug-far").open({ prefills = { paths = vim.fn.expand("%") } })
+			end,
+			mode = { "n", "v" },
+			desc = "Search and Replace (current buf)",
+		},
+		{
+			"<leader>R",
+			function()
 				require("grug-far").open()
 			end,
 			mode = { "n", "v" },
