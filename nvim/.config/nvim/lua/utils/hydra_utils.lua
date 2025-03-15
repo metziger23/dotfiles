d753a73692594b3e9d64ae3f0b6c979dcf4a2f73
metziger23 <metziger23@gmail.com>
2025-03-15 22:54:01 +0300

    better which-key description inside hydra

--- nvim/.config/nvim/lua/utils/hydra_utils.lua
+++ nvim/.config/nvim/lua/utils/hydra_utils.lua
@@ -2,6 +2,8 @@ local M = {}
 
 local function single_hydra_setup(modes, desc, prev_keymap, next_keymap, prev_func, next_func, opts)
 	local Hydra = require("hydra")
+	local next_desc = "Go to next " .. desc
+	local prev_desc = "Go to previous " .. desc
 	return Hydra({
 		name = "Go to next/previous " .. desc,
 		mode = modes,
@@ -10,10 +12,10 @@ local function single_hydra_setup(modes, desc, prev_keymap, next_keymap, prev_fu
 			color = "pink",
 		},
 		heads = {
-			{ prev_keymap, prev_func },
-			{ "<PageUp>", prev_func },
-			{ next_keymap, next_func },
-			{ "<PageDown>", next_func },
+			{ prev_keymap, prev_func, { desc = prev_desc } },
+			{ "<PageUp>", prev_func, { desc = prev_desc } },
+			{ next_keymap, next_func, { desc = next_desc } },
+			{ "<PageDown>", next_func, { desc = next_desc } },
 		},
 	})
 end
@@ -22,7 +24,7 @@ local function single_keymap_setup(is_next, modes, desc, prev_keymap, next_keyma
 	local cur_keymap = is_next and next_keymap or prev_keymap
 	local cur_func = is_next and next_func or prev_func
 	local direction_desc = is_next and "next" or "previous"
-  local complete_direction_desc = "Go to " .. direction_desc .. " " .. desc
+	local complete_direction_desc = "Go to " .. direction_desc .. " " .. desc
 	opts.desc = complete_direction_desc
 	vim.keymap.set(modes, cur_keymap, function()
 		cur_func()
@@ -32,9 +34,9 @@ local function single_keymap_setup(is_next, modes, desc, prev_keymap, next_keyma
 end
 
 function M.setup_bidirectional_hydra(modes, desc, prev_keymap, next_keymap, prev_func, next_func, opts)
-  opts = opts or {}
-  single_keymap_setup(false, modes, desc, prev_keymap, next_keymap, prev_func, next_func, opts)
-  single_keymap_setup(true, modes, desc, prev_keymap, next_keymap, prev_func, next_func, opts)
+	opts = opts or {}
+	single_keymap_setup(false, modes, desc, prev_keymap, next_keymap, prev_func, next_func, opts)
+	single_keymap_setup(true, modes, desc, prev_keymap, next_keymap, prev_func, next_func, opts)
 end
 
 return M
