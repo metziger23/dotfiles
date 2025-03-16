b3071c6ad3f285da9a0d4becb734cf16865ed0e8
metziger23 <metziger23@gmail.com>
2025-03-16 10:26:01 +0300

    formatted dap.lua

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -1,7 +1,8 @@
 return {
-  'mfussenegger/nvim-dap',
-  config = function() end,
-  keys = {
+	"mfussenegger/nvim-dap",
+	config = function() end,
+	keys = {
+    -- stylua: ignore start
     { '<leader>dB', function() require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = 'Breakpoint Condition' },
     { '<leader>db', function() require('dap').toggle_breakpoint() end, desc = 'Toggle Breakpoint' },
     { '<leader>dc', function() require('dap').continue() end, desc = 'Continue' },
@@ -18,84 +19,105 @@ return {
     { '<leader>ds', function() require('dap').session() end, desc = 'Session' },
     { '<leader>dt', function() require('dap').terminate() end, desc = 'Terminate' },
     { '<leader>dw', function() require('dap.ui.widgets').hover() end, desc = 'Widgets' },
-  },
-  opts = function()
-    local dap = require('dap')
-    dap.adapters.codelldb = {
-      type = 'server',
-      port = '${port}',
-      executable = {
-        command = 'codelldb',
-        args = { '--port', '${port}' },
-      },
-    }
-    dap.configurations.cpp = {
-      {
-        name = 'Launch file',
-        type = 'codelldb',
-        request = 'launch',
-        program = function() return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file') end,
-        cwd = '${workspaceFolder}',
-        stopOnEntry = false,
-        args = {},
-      },
-      {
-        name = 'Attach to process',
-        type = 'codelldb',
-        request = 'attach',
-        pid = require('dap.utils').pick_process,
-        cwd = '${workspaceFolder}',
-      },
-    }
-    dap.configurations.c = dap.configurations.cpp
-    require("overseer").enable_dap(true)
-  end,
-  dependencies = {
-    "stevearc/overseer.nvim",
-    {
-      "rcarriga/nvim-dap-ui",
-      dependencies = {
-        "nvim-neotest/nvim-nio",
-      },
-      keys = {
-        { '<leader>du', function() require('dapui').toggle({}) end, desc = 'Dap UI' },
-        { '<leader>de', function() require('dapui').eval() end, desc = 'Eval', mode = { 'n', 'v' } },
-      },
-      opts = {},
-      config = function(_, opts)
-        local dap = require('dap')
-        local dapui = require('dapui')
-        dapui.setup(opts)
-        dap.listeners.after.event_initialized['dapui_config'] = function() dapui.open({}) end
-        dap.listeners.before.event_terminated['dapui_config'] = function() dapui.close({}) end
-        dap.listeners.before.event_exited['dapui_config'] = function() dapui.close({}) end
-      end,
-    },
+		-- stylua: ignore end
+	},
+	opts = function()
+		local dap = require("dap")
+		dap.adapters.codelldb = {
+			type = "server",
+			port = "${port}",
+			executable = {
+				command = "codelldb",
+				args = { "--port", "${port}" },
+			},
+		}
+		dap.configurations.cpp = {
+			{
+				name = "Launch file",
+				type = "codelldb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopOnEntry = false,
+				args = {},
+			},
+			{
+				name = "Attach to process",
+				type = "codelldb",
+				request = "attach",
+				pid = require("dap.utils").pick_process,
+				cwd = "${workspaceFolder}",
+			},
+		}
+		dap.configurations.c = dap.configurations.cpp
+		require("overseer").enable_dap(true)
+	end,
+	dependencies = {
+		"stevearc/overseer.nvim",
+		{
+			"rcarriga/nvim-dap-ui",
+			dependencies = {
+				"nvim-neotest/nvim-nio",
+			},
+			keys = {
+				{
+					"<leader>du",
+					function()
+						require("dapui").toggle({})
+					end,
+					desc = "Dap UI",
+				},
+				{
+					"<leader>de",
+					function()
+						require("dapui").eval()
+					end,
+					desc = "Eval",
+					mode = { "n", "v" },
+				},
+			},
+			opts = {},
+			config = function(_, opts)
+				local dap = require("dap")
+				local dapui = require("dapui")
+				dapui.setup(opts)
+				dap.listeners.after.event_initialized["dapui_config"] = function()
+					dapui.open({})
+				end
+				dap.listeners.before.event_terminated["dapui_config"] = function()
+					dapui.close({})
+				end
+				dap.listeners.before.event_exited["dapui_config"] = function()
+					dapui.close({})
+				end
+			end,
+		},
 
-    -- virtual text for the debugger
-    {
-      'theHamsta/nvim-dap-virtual-text',
-      -- dependencies = 'mfussenegger/nvim-dap',
-      opts = {},
-    },
-    -- which key integration
-    {
-      'folke/which-key.nvim',
-      opts = {
-        defaults = {
-          ['<leader>d'] = { name = '+debug' },
-        },
-      },
-    },
+		-- virtual text for the debugger
+		{
+			"theHamsta/nvim-dap-virtual-text",
+			-- dependencies = 'mfussenegger/nvim-dap',
+			opts = {},
+		},
+		-- which key integration
+		{
+			"folke/which-key.nvim",
+			opts = {
+				defaults = {
+					["<leader>d"] = { name = "+debug" },
+				},
+			},
+		},
 
-    {
-      "jay-babu/mason-nvim-dap.nvim",
-      dependencies = "mason.nvim",
-      cmd = { "DapInstall", "DapUninstall" },
-      opts = {
-        ensure_installed = { "codelldb"}
-      },
-    },
-
-  }
+		{
+			"jay-babu/mason-nvim-dap.nvim",
+			dependencies = "mason.nvim",
+			cmd = { "DapInstall", "DapUninstall" },
+			opts = {
+				ensure_installed = { "codelldb" },
+			},
+		},
+	},
 }
