e239b9050febdef4fb27f24d0741b9b70fb503dc
metziger23 <metziger23@gmail.com>
2025-03-16 23:55:35 +0300

    f-keys for dap

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -13,22 +13,22 @@ return {
 	end,
 	keys = {
     -- stylua: ignore start
-    { '<leader>dB', function() require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = 'Breakpoint Condition' },
-    { '<leader>db', function() require('dap').toggle_breakpoint() end, desc = 'Toggle Breakpoint' },
-    { '<leader>dc', function() require('dap').continue() end, desc = 'Continue' },
-    { '<leader>dC', function() require('dap').run_to_cursor() end, desc = 'Run to Cursor' },
-    { '<leader>dg', function() require('dap').goto_() end, desc = 'Go to line (no execute)' },
-    { '<leader>di', function() require('dap').step_into() end, desc = 'Step Into' },
-    { '<leader>dj', function() require('dap').down() end, desc = 'Down' },
-    { '<leader>dk', function() require('dap').up() end, desc = 'Up' },
-    { '<leader>dl', function() require('dap').run_last() end, desc = 'Run Last' },
-    { '<leader>do', function() require('dap').step_out() end, desc = 'Step Out' },
-    { '<leader>dO', function() require('dap').step_over() end, desc = 'Step Over' },
-    { '<leader>dp', function() require('dap').pause() end, desc = 'Pause' },
-    { '<leader>dr', function() require('dap').repl.toggle() end, desc = 'Toggle REPL' },
-    { '<leader>ds', function() require('dap').session() end, desc = 'Session' },
-    { '<leader>dt', function() require('dap').terminate() end, desc = 'Terminate' },
-    { '<leader>dw', function() require('dap.ui.widgets').hover() end, desc = 'Widgets' },
+    { '<F1>', function() require('dap').toggle_breakpoint() end, desc = 'Toggle Breakpoint' },
+    { '<F2>', function() require('dap').run_to_cursor() end, desc = 'Run to Cursor' },
+    { '<F3>', function() require('dap').goto_() end, desc = 'Go to line (no execute)' },
+    { '<F4>', function() require('dap').terminate() end, desc = 'Terminate' },
+    { '<F5>', function() require('dap').continue() end, desc = 'Continue' },
+    { '<F6>', function() require('dap').pause() end, desc = 'Pause' },
+    { '<F7>', function() require('dap').step_into() end, desc = 'Step Into' },
+    { '<F8>', function() require('dap').step_out() end, desc = 'Step Out' },
+    { '<F9>', function() require('dap').step_over() end, desc = 'Step Over' },
+    { '<F10>', function() require('dap').down() end, desc = 'Down' },
+    { '<F11>', function() require('dap').up() end, desc = 'Up' },
+    { '<F12>', function() require('dap').run_last() end, desc = 'Run Last' },
+    { '<F13>', function() require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = 'Breakpoint Condition' },
+    { '<F14>', function() require('dap.ui.widgets').hover() end, desc = 'Widgets' },
+    { '<F15>', function() require('dap').repl.toggle() end, desc = 'Toggle REPL' },
+    { '<F16>', function() require('dap').session() end, desc = 'Session' },
 		-- stylua: ignore end
 	},
 	opts = function()
@@ -80,14 +80,14 @@ return {
 			},
 			keys = {
 				{
-					"<leader>du",
+					"<F17>",
 					function()
 						require("dapui").toggle({})
 					end,
 					desc = "Dap UI",
 				},
 				{
-					"<leader>de",
+					"<F18>",
 					function()
 						require("dapui").eval()
 					end,
@@ -118,16 +118,6 @@ return {
 			-- dependencies = 'mfussenegger/nvim-dap',
 			opts = {},
 		},
-		-- which key integration
-		{
-			"folke/which-key.nvim",
-			opts = {
-				defaults = {
-					["<leader>d"] = { name = "+debug" },
-				},
-			},
-		},
-
 		{
 			"jay-babu/mason-nvim-dap.nvim",
 			dependencies = "mason.nvim",
