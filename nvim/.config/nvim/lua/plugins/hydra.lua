4048a8104a787aaf4f1eb3d6c071e1e25d191c68
metziger23 <metziger23@gmail.com>
2025-03-17 20:59:42 +0300

    added fix for hydra to make it work on 0.11 nightly

--- nvim/.config/nvim/lua/plugins/hydra.lua
+++ nvim/.config/nvim/lua/plugins/hydra.lua
@@ -1,4 +1,6 @@
 return {
-	"nvimtools/hydra.nvim",
+	-- FIX: https://github.com/nvimtools/hydra.nvim/pull/47
+	-- change to "nvimtools/hydra.nvim", later
+	"cathyprime/hydra.nvim",
 	lazy = true,
 }
