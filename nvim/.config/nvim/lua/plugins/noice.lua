81d206e84377a7baf5b92a346f2cb9fa756d2535
metziger23 <metziger23@gmail.com>
2025-03-22 22:17:44 +0300

    mini view for noice

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -5,7 +5,18 @@ return {
 		presets = {
 			lsp_doc_border = true, -- add a border to hover docs and signature help
 		},
+		messages = {
+			view = "mini", -- default view for messages
+			view_error = "mini", -- view for errors
+			view_warn = "mini", -- view for warnings
+			view_history = "mini", -- view for :messages
+			view_search = "mini", -- view for search count messages. Set to `false` to disable
+		},
+		notify = {
+			view = "mini",
+		},
 		lsp = {
+			view = "mini",
 			signature = {
 				enabled = true,
 				auto_open = {
