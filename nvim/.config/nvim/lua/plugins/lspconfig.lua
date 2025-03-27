e7ccb273e2f090aa5f1a604685f384c2c8bc2b86
metziger23 <metziger23@gmail.com>
2025-03-28 00:09:18 +0300

    use blink-cmp instead of nvim-cmp

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -4,7 +4,7 @@ return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
+		"saghen/blink.cmp",
 		{ "antosha417/nvim-lsp-file-operations", config = true },
 		"mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
@@ -24,9 +24,6 @@ return {
 
 		-- import lspconfig plugin
 		local lspconfig = require("lspconfig")
-
-		-- import cmp-nvim-lsp plugin
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
 		local keymap = vim.keymap -- for conciseness
 
@@ -130,7 +127,6 @@ return {
 		end
 
 		-- used to enable autocompletion (assign to every lsp server config)
-		local capabilities = cmp_nvim_lsp.default_capabilities()
 
 		vim.diagnostic.config({
 			signs = {
@@ -146,6 +142,8 @@ return {
 			-- underline = true,
 			-- etc.
 		})
+
+		local capabilities = require("blink.cmp").get_lsp_capabilities()
 
 		-- configure bashls server
 		lspconfig["bashls"].setup({
