f2c3ee4b65a47ae1efe94ad36ab3f30a9988bee3
metziger23 <metziger23@gmail.com>
2025-03-27 09:49:57 +0300

    added nvim-ts-context-commentstring integration for Comment

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,6 +1,7 @@
 -- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
 return {
 	"numToStr/Comment.nvim",
+  dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
 	keys = {
 		{ "gcc", mode = "n", desc = "Comment toggle current line" },
 		{ "gc", mode = { "n", "o" }, desc = "Comment toggle linewise" },
@@ -9,7 +10,9 @@ return {
 		{ "gb", mode = { "n", "o" }, desc = "Comment toggle blockwise" },
 		{ "gb", mode = "x", desc = "Comment toggle blockwise (visual)" },
 	},
-	config = function(_, opts)
-		require("Comment").setup(opts)
-	end,
+  config = function()
+    require("Comment").setup({
+      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+    })
+  end,
 }
