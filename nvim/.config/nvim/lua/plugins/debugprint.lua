48dbdf1d77b075739cbe178cf222dbfdba5e5727
mikhail <mikhail@mikhails-Mac-mini.local>
2025-05-26 13:43:10 +0300

    added debugprint

---
+++ nvim/.config/nvim/lua/plugins/debugprint.lua
@@ -0,0 +1,70 @@
+-- stylua: ignore start
+local colors = {
+	"red", "green", "blue", "orange", "purple",
+	"teal", "pink", "lime", "white", "navy", "olive",
+	 "silver", "gray", "gold", "cyan", "magenta",
+  "brown", "darkgreen", "skyblue", "indigo",
+  "coral", "yellow", "violet", "firebrick",
+	 "royalblue", "sienna", "tomato", "slateblue",
+}
+-- stylua: ignore end
+
+return {
+	"andrewferrier/debugprint.nvim", -- opts = {},
+
+	dependencies = {
+		-- "echasnovski/mini.nvim", -- Optional: Needed for line highlighting (full mini.nvim plugin)
+		-- ... or ...
+		-- "echasnovski/mini.hipatterns", -- Optional: Needed for line highlighting ('fine-grained' hipatterns plugin)
+
+		-- "ibhagwan/fzf-lua", -- Optional: If you want to use the `:Debugprint search` command with fzf-lua
+		-- "nvim-telescope/telescope.nvim", -- Optional: If you want to use the `:Debugprint search` command with telescope.nvim
+		"folke/snacks.nvim", -- Optional: If you want to use the `:Debugprint search` command with snacks.nvim
+	},
+
+	lazy = false, -- Required to make line highlighting work before debugprint is first used
+	version = "*", -- Remove if you DON'T want to use the stable version
+	config = function()
+		local counter = require("debugprint.counter")
+		local default_display_counter = counter.default_display_counter
+
+		counter.default_display_counter = function()
+			local default_result = default_display_counter()
+			local default_result_number = tonumber(default_result:match("%[(%d+)%]"))
+			---@diagnostic disable-next-line: param-type-mismatch
+			local color_number = math.fmod(default_result_number, #colors)
+			return colors[color_number]
+		end
+
+		require("debugprint").setup({
+			display_location = false,
+			print_tag = "",
+			keymaps = {
+				normal = {
+					plain_below = "<M-i>p",
+					plain_above = "<M-i>P",
+					variable_below = "<M-i>v",
+					variable_above = "<M-i>V",
+					variable_below_alwaysprompt = "",
+					variable_above_alwaysprompt = "",
+					surround_plain = "<M-i>sp",
+					surround_variable = "<M-i>sv",
+					surround_variable_alwaysprompt = "",
+					textobj_below = "<M-i>o",
+					textobj_above = "<M-i>O",
+					textobj_surround = "<M-i>so",
+					toggle_comment_debug_prints = "",
+					delete_debug_prints = "",
+				},
+				insert = {
+					plain = "<M-i>p",
+					variable = "<M-i>v",
+				},
+				visual = {
+					variable_below = "<M-i>v",
+					variable_above = "<M-i>V",
+				},
+			},
+		})
+	end,
+}
