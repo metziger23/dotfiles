1285ddf86fa4a138a432777ef118e4f78f8ffa8d
metziger23 <metziger23@gmail.com>
2025-06-01 17:26:41 +0300

    use dropbar instead of barbecue

---
+++ nvim/.config/nvim/lua/plugins/dropbar.lua
@@ -0,0 +1,14 @@
+-- 'Bekaboo/dropbar.nvim',
+return {
+	"Bekaboo/dropbar.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons", -- optional dependency
+	},
+	opts = {
+		menu = {
+			win_configs = {
+				border = "rounded",
+			},
+		},
+	},
+}
