100ad0e54d7d461428d614bcea39d2bfff311dc1
mikhail <mikhail@mikhails-Mac-mini.local>
2025-07-17 23:45:02 +0300

    fixed bugs in langmap
    fixed issue with "

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -52,10 +52,10 @@ opt.showmode = false
 -- global statusline
 opt.laststatus = 3
 
-local ru_upper = 'ЦКЛБЙЪЫЯЭФЗВНТДИАОЕСХПРМГ\\"ЬУЮШ'
-local en_upper = 'QWFPBJLUY\\"ARSTGMNEIOZXCDVKH\\<>?'
-local ru_lower = "цклбйъыяэфзвнтдиаоесхпрмг'ьуюш"
-local en_lower = "qwfpbjluy'arstgmneiozxcdvkh\\,./"
+local ru_upper = "ЦКЛБЙЪЫЯЭФЗВНТДИАОЕСХПРМГЬУЮШ"
+local en_upper = 'QWFPBJLUY\\"ARSTGMNEIOZXCDVH<>?'
+local ru_lower = "цклбйъыяэфзвнтдиаоесхпрмгьуюш"
+local en_lower = "qwfpbjluy'arstgmneiozxcdvh\\,./"
 opt.langmap = ru_upper .. ";" .. en_upper .. "," .. ru_lower .. ";" .. en_lower
 
 opt.exrc = true
