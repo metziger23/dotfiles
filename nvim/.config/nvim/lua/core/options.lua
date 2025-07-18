4c145a19cd2e2b6e9cc0bc425cfed04809de10ae
mikhail <mikhail@mikhails-Mac-mini.local>
2025-07-18 10:45:21 +0300

    returned stay-centered

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -39,7 +39,7 @@ opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 opt.swapfile = false
 
 -- Always keep 8 lines above/below cursor unless at start/end of file
-opt.scrolloff = 999
+-- opt.scrolloff = 8 NOTE: disaled since I'm using stay-centered plugin
 
 opt.cursorline = true
 
