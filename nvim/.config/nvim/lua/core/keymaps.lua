4344fad238050cff57bacd1a7a224cd500920647
mikhail <mikhail@mikhails-Mac-mini.local>
2025-07-29 21:48:36 +0300

    removed builtin grt keymap

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -31,6 +31,7 @@ vim.api.nvim_del_keymap("n", "gra")
 vim.api.nvim_del_keymap("x", "gra")
 vim.api.nvim_del_keymap("n", "grr")
 vim.api.nvim_del_keymap("n", "gri")
+vim.api.nvim_del_keymap("n", "grt")
 
 vim.keymap.set("x", "z/", "<C-\\><C-n>`</\\%V", { desc = "Search forward within visual selection" })
 vim.keymap.set("x", "z?", "<C-\\><C-n>`>?\\%V", { desc = "Search backward within visual selection" })
