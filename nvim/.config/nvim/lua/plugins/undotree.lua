564b609a6bd8a3c2cad43ac96aa50c545d750985
metziger23 <metziger23@gmail.com>
2025-07-31 21:49:18 +0300

    added descriptions for undotree custom keymaps

--- nvim/.config/nvim/lua/plugins/undotree.lua
+++ nvim/.config/nvim/lua/plugins/undotree.lua
@@ -7,10 +7,15 @@ return {
 		vim.g.undotree_WindowLayout = 2
 		vim.g.Undotree_CustomMap = function()
       -- stylua: ignore start  
-      vim.api.nvim_buf_set_keymap(0, "n", "<S-Down>", "<plug>UndotreePreviousState", { noremap = true, silent = true })
-			vim.api.nvim_buf_set_keymap(0, "n", "<S-Up>", "<plug>UndotreeNextState", { noremap = true, silent = true })
-      vim.api.nvim_buf_set_keymap(0, "n", "<C-Down>", "<plug>UndotreePreviousSavedState", { noremap = true, silent = true })
-      vim.api.nvim_buf_set_keymap(0, "n", "<C-Up>", "<plug>UndotreeNextSavedState", { noremap = true, silent = true })
+      local opts = { noremap = true, silent = true }
+      opts.desc = "Undotree Previous State"
+      vim.api.nvim_buf_set_keymap(0, "n", "<S-Down>", "<plug>UndotreePreviousState", opts)
+      opts.desc = "Undotree Next State"
+			vim.api.nvim_buf_set_keymap(0, "n", "<S-Up>", "<plug>UndotreeNextState", opts)
+      opts.desc = "Undotree Previous Saved State"
+      vim.api.nvim_buf_set_keymap(0, "n", "<C-Down>", "<plug>UndotreePreviousSavedState", opts)
+      opts.desc = "Undotree Next Saved State"
+      vim.api.nvim_buf_set_keymap(0, "n", "<C-Up>", "<plug>UndotreeNextSavedState", opts)
 			-- stylua: ignore end
 		end
 	end,
