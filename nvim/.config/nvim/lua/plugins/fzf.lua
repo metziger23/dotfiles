bbe3a68d757a6568b17735ae31c7351b632c1d38
metziger23 <metziger23@gmail.com>
2025-07-31 21:36:33 +0300

    call register_ui_select for fzf-lua

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -84,16 +84,16 @@ return {
       end, desc = "Fzf zoxide tcd",
     }
   },
-	-- init = function()
-	-- 	require("fzf-lua").register_ui_select(function(_, items)
-	-- 		local min_h, max_h = 0.15, 0.70
-	-- 		local h = (#items + 4) / vim.o.lines
-	-- 		if h < min_h then
-	-- 			h = min_h
-	-- 		elseif h > max_h then
-	-- 			h = max_h
-	-- 		end
-	-- 		return { winopts = { height = h, width = 0.60, row = 0.40 } }
-	-- 	end)
-	-- end,
+	init = function()
+		require("fzf-lua").register_ui_select(function(_, items)
+			local min_h, max_h = 0.15, 0.70
+			local h = (#items + 4) / vim.o.lines
+			if h < min_h then
+				h = min_h
+			elseif h > max_h then
+				h = max_h
+			end
+			return { winopts = { height = h, width = 0.60, row = 0.40 } }
+		end)
+	end,
 }
