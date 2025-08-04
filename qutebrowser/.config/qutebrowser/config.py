d5b3adc307808480e5c1cd5db418eeb31dfd6e1d
metziger23 <metziger23@gmail.com>
2025-08-04 17:16:00 +0300

    updated qutebrowser config

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,3 +1,5 @@
+import platform
+
 config.load_autoconfig()
 
 c.url.start_pages = ['https://www.google.com/']
@@ -6,20 +8,25 @@ c.url.searchengines = {
     'DEFAULT':  'https://google.com/search?hl=en&q={}',
 }
 
-config.bind("<Ctrl-K>", "completion-item-focus prev", mode="command")
-config.bind("<Ctrl-J>", "completion-item-focus next", mode="command")
-
-for i in range (1, 10):
-    meta = '<Meta-' + str(i) + '>'
-    alt = '<Alt-' + str(i) + '>'
-    c.bindings.key_mappings[meta] = alt
+config.bind("<Home>", "rl-beginning-of-line", mode="command")
+config.bind("<End>", "rl-end-of-line", mode="command")
 
 config.bind('<Ctrl-L>', 'search', mode='normal')
 
 c.colors.webpage.darkmode.enabled = True
 c.content.javascript.clipboard = "access"
 
-config.bind("<Ctrl-W>", "fake-key <Ctrl-backspace>", mode="insert")
+if platform.system() == 'Darwin':
+    config.bind("<Ctrl-V>", "insert-text -- {clipboard}", mode="insert")
+    config.bind("<Ctrl-Shift-V>", "insert-text -- {clipboard}", mode="insert")
+    config.bind("<Ctrl-V>", "cmd-set-text -a {clipboard}", mode="command")
+    config.bind("<Ctrl-Shift-V>", "cmd-set-text -a {clipboard}", mode="command")
+    config.bind("<Ctrl-W>", "fake-key <Alt-backspace>", mode="insert")
+    config.bind("<Ctrl-Left>", "fake-key <Alt-Left>", mode="insert")
+    config.bind("<Ctrl-Right>", "fake-key <Alt-Right>", mode="insert") 
+else:
+    config.bind("<Ctrl-W>", "fake-key <Ctrl-backspace>", mode="insert")
+
 config.bind("<Ctrl-H>", "fake-key <backspace>", mode="insert")
 config.bind("<Ctrl-P>", "fake-key <Up>", mode="insert")
 config.bind("<Ctrl-N>", "fake-key <Down>", mode="insert")
@@ -32,6 +39,19 @@ config.bind("<Ctrl-U>", "fake-key <Shift-Home>;; cmd-later 3 fake-key <Delete>",
 config.bind("<Ctrl-K>", "fake-key <Shift-End><Delete>", mode="insert")
 config.bind("<Ctrl-A>", "fake-key <Home>", mode="insert")
 
+config.bind("<Down>", "fake-key <j>", mode="normal")
+config.bind("<Up>", "fake-key <k>", mode="normal")
+
+config.bind("<Shift-Left>", "tab-prev", mode='normal')
+config.bind("<Shift-Right>", "tab-next", mode='normal')
+config.bind("<Shift-Up>", "tab-prev", mode='normal')
+config.bind("<Shift-Down>", "tab-next", mode='normal')
+
+config.bind("<Left>", "scroll left", mode='normal')
+config.bind("<Right>", "scroll right", mode='normal')
+config.bind("<Up>", "scroll up", mode='normal')
+config.bind("<Down>", "scroll down", mode='normal')
+
 c.qt.args = ['ignore-gpu-blocklist', 'enable-gpu-rasterization',
              'enable-accelerated-video-decode', 'enable-quic', 'enable-zero-copy']
 
@@ -40,3 +60,24 @@ config.bind("cn", "tab-only --prev", mode="normal")  # close next
 
 
 c.fonts.default_size = "16px"
+
+en_keys = "qwfpbjluy'arstgmneiozxcdvkh,./" + 'QWFPBJLUY"ARSTGMNEIOZXCDVKH<>?'
+ru_keys = "цклбйъыяэфзвнтдиаоесхпрмг'ьуюш" + 'ЦКЛБЙЪЫЯЭФЗВНТДИАОЕСХПРМГ"ЬУЮШ'
+c.bindings.key_mappings.update(dict(zip(ru_keys, en_keys)))
+
+c.scrolling.smooth = True
+
+c.tabs.position = "left"
+
+c.auto_save.session = True
+
+# Tabs bar colors
+c.colors.tabs.bar.bg = "#000000"  # Black background for the tabs bar
+c.colors.tabs.even.bg = "#000000"  # Even tabs background
+c.colors.tabs.odd.bg = "#000000"   # Odd tabs background
+c.colors.tabs.selected.even.bg = "#444444"  # Selected even tab
+c.colors.tabs.selected.odd.bg = "#444444"   # Selected odd tab
+c.colors.tabs.indicator.start = "#000000"   # Gradient start
+c.colors.tabs.indicator.stop = "#000000"    # Gradient end
+  
+c.scrolling.bar = "always"  # Options: "always", "never", "when-searching"
