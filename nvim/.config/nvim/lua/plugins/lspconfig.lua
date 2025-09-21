b5c48d58d6d285a20ba10ed62e320fe48b242726
metziger23 <metziger23@gmail.com>
2025-09-21 15:45:08 +0300

    use vim.lsp.config in lspconfig

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,5 +1,21 @@
 local hydra_utils = require("../utils/hydra_utils")
 
+--- Implements the off-spec textDocument/switchSourceHeader method.
+--- @param buf integer
+local function switch_source_header(client, buf)
+	client:request("textDocument/switchSourceHeader", vim.lsp.util.make_text_document_params(buf), function(err, result)
+		if err then
+			vim.notify(err.message, vim.log.levels.ERROR)
+			return
+		end
+		if not result then
+			vim.notify("Corresponding file could not be determined", vim.log.levels.WARN)
+			return
+		end
+		vim.cmd.edit(vim.uri_to_fname(result))
+	end)
+end
+
 return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPre", "BufNewFile" },
@@ -22,104 +38,111 @@ return {
 		})
 		require("lspconfig.ui.windows").default_options.border = "rounded"
 
-		-- import lspconfig plugin
-		local lspconfig = require("lspconfig")
-
 		local keymap = vim.keymap -- for conciseness
 
-		local opts = { noremap = true, silent = true }
-		local on_attach = function(client, bufnr)
-			opts.buffer = bufnr
-			local fzf_lua = require("fzf-lua")
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+			callback = function(ev)
+				local lsp = vim.lsp
+				local completion = lsp.completion
+				local ms = lsp.protocol.Methods
 
-			opts.desc = "Signature Help"
-			keymap.set({ "n", "i", "s" }, "<c-s>", function()
-				-- NOTE: for some reason the border gets rouned only when this
-				-- is called with "function()"
-				vim.lsp.buf.signature_help()
-			end, opts)
+				local client = lsp.get_client_by_id(ev.data.client_id)
+				if not client or not client:supports_method(ms.textDocument_completion) then
+					return
+				end
+				-- Buffer local mappings.
+				-- See `:help vim.lsp.*` for documentation on any of the below functions
+				local opts = { buffer = ev.buf, silent = true }
+				local fzf_lua = require("fzf-lua")
 
-			opts.desc = "Go to declaration"
-			keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-			opts.desc = "Goto definition"
-			keymap.set("n", "gd", fzf_lua.lsp_definitions, opts)
-			keymap.set("n", "gI", fzf_lua.lsp_implementations, opts)
-			opts.desc = "Goto type"
-			keymap.set("n", "gy", fzf_lua.lsp_typedefs, opts)
-			opts.desc = "Goto References"
-			keymap.set("n", "gr", fzf_lua.lsp_references, opts)
-			opts.desc = "Goto Line Diagnostics"
-			keymap.set("n", "gl", vim.diagnostic.open_float, opts)
+				opts.desc = "Signature Help"
+				keymap.set({ "n", "i", "s" }, "<c-s>", function()
+					-- NOTE: for some reason the border gets rouned only when this
+					-- is called with "function()"
+					vim.lsp.buf.signature_help()
+				end, opts)
 
-			opts.desc = "Lsp Implementations"
-			keymap.set("n", "li", fzf_lua.lsp_implementations, opts)
-			opts.desc = "Lsp Document Symbols"
-			keymap.set("n", "ls", fzf_lua.lsp_document_symbols, opts)
-			opts.desc = "Lsp Workspace Symbols"
-			keymap.set("n", "lw", fzf_lua.lsp_live_workspace_symbols, opts)
-			opts.desc = "Lsp Workspace Symbols (live query)"
-			keymap.set("n", "ll", fzf_lua.lsp_live_workspace_symbols, opts)
-			opts.desc = "Lsp Document Diagnostics"
-			keymap.set("n", "ld", fzf_lua.diagnostics_document, opts)
-			opts.desc = "Lsp Workspace Diagnostics"
-			keymap.set("n", "lD", fzf_lua.diagnostics_workspace, opts)
+				opts.desc = "Go to declaration"
+				keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+				opts.desc = "Goto definition"
+				keymap.set("n", "gd", fzf_lua.lsp_definitions, opts)
+				keymap.set("n", "gI", fzf_lua.lsp_implementations, opts)
+				opts.desc = "Goto type"
+				keymap.set("n", "gy", fzf_lua.lsp_typedefs, opts)
+				opts.desc = "Goto References"
+				keymap.set("n", "gr", fzf_lua.lsp_references, opts)
+				opts.desc = "Goto Line Diagnostics"
+				keymap.set("n", "gl", vim.diagnostic.open_float, opts)
 
-			if client.supports_method("callHierarchy/incomingCalls") then
-				opts.desc = "Lsp incoming calls"
-				keymap.set("n", "lc", fzf_lua.lsp_incoming_calls, opts)
-			end
+				opts.desc = "Lsp Implementations"
+				keymap.set("n", "li", fzf_lua.lsp_implementations, opts)
+				opts.desc = "Lsp Document Symbols"
+				keymap.set("n", "ls", fzf_lua.lsp_document_symbols, opts)
+				opts.desc = "Lsp Workspace Symbols"
+				keymap.set("n", "lw", fzf_lua.lsp_live_workspace_symbols, opts)
+				opts.desc = "Lsp Workspace Symbols (live query)"
+				keymap.set("n", "ll", fzf_lua.lsp_live_workspace_symbols, opts)
+				opts.desc = "Lsp Document Diagnostics"
+				keymap.set("n", "ld", fzf_lua.diagnostics_document, opts)
+				opts.desc = "Lsp Workspace Diagnostics"
+				keymap.set("n", "lD", fzf_lua.diagnostics_workspace, opts)
 
-			if client.supports_method("callHierarchy/outgoingCalls") then
-				opts.desc = "Lsp outgoing calls"
-				keymap.set("n", "lC", fzf_lua.lsp_outgoing_calls, opts)
-			end
+				if client:supports_method("callHierarchy/incomingCalls", ev.buf) then
+					opts.desc = "Lsp incoming calls"
+					keymap.set("n", "lc", fzf_lua.lsp_incoming_calls, opts)
+				end
 
-			opts.desc = "Lsp rename"
-			keymap.set("n", "lr", function()
-				vim.lsp.buf.rename()
-			end, opts)
-			opts.desc = "Lsp Code action"
-			keymap.set({ "n", "x" }, "la", function()
-				vim.lsp.buf.code_action()
-			end, opts)
-			opts.desc = "Lsp Source action"
-			keymap.set({ "n", "x" }, "lA", function()
-				vim.lsp.buf.code_action({ context = { only = { "source" }, diagnostics = {} } })
-			end, opts)
-			opts.desc = "Format"
-			keymap.set("n", "lf", function()
-				require("conform").format()
-			end, opts)
-			opts.desc = "Format selection"
-			keymap.set("v", "lf", function()
-				require("conform").format()
-			end, opts)
+				if client:supports_method("callHierarchy/outgoingCalls", ev.buf) then
+					opts.desc = "Lsp outgoing calls"
+					keymap.set("n", "lC", fzf_lua.lsp_outgoing_calls, opts)
+				end
 
-			opts.desc = "Hover information"
-			keymap.set("n", "K", function()
-				vim.lsp.buf.hover()
-			end, opts)
+				opts.desc = "Lsp rename"
+				keymap.set("n", "lr", function()
+					vim.lsp.buf.rename()
+				end, opts)
+				opts.desc = "Lsp Code action"
+				keymap.set({ "n", "x" }, "la", function()
+					vim.lsp.buf.code_action()
+				end, opts)
+				opts.desc = "Lsp Source action"
+				keymap.set({ "n", "x" }, "lA", function()
+					vim.lsp.buf.code_action({ context = { only = { "source" }, diagnostics = {} } })
+				end, opts)
+				opts.desc = "Format"
+				keymap.set("n", "lf", function()
+					require("conform").format()
+				end, opts)
+				opts.desc = "Format selection"
+				keymap.set("v", "lf", function()
+					require("conform").format()
+				end, opts)
 
-			hydra_utils.setup_bidirectional_hydra("n", "diagnostic", "[d", "]d", function()
-				vim.diagnostic.goto_prev({ severity = nil })
-			end, function()
-				vim.diagnostic.goto_next({ severity = nil })
-			end, opts)
+				opts.desc = "Hover information"
+				keymap.set("n", "K", function()
+					vim.lsp.buf.hover()
+				end, opts)
 
-			hydra_utils.setup_bidirectional_hydra("n", "error", "[e", "]e", function()
-				vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity["ERROR"] })
-			end, function()
-				vim.diagnostic.goto_next({ severity = vim.diagnostic.severity["ERROR"] })
-			end, opts)
+				hydra_utils.setup_bidirectional_hydra("n", "diagnostic", "[d", "]d", function()
+					vim.diagnostic.goto_prev({ severity = nil })
+				end, function()
+					vim.diagnostic.goto_next({ severity = nil })
+				end, opts)
 
-			hydra_utils.setup_bidirectional_hydra("n", "warning", "[w", "]w", function()
-				vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity["WARN"] })
-			end, function()
-				vim.diagnostic.goto_next({ severity = vim.diagnostic.severity["WARN"] })
-			end, opts)
-		end
+				hydra_utils.setup_bidirectional_hydra("n", "error", "[e", "]e", function()
+					vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity["ERROR"] })
+				end, function()
+					vim.diagnostic.goto_next({ severity = vim.diagnostic.severity["ERROR"] })
+				end, opts)
 
-		-- used to enable autocompletion (assign to every lsp server config)
+				hydra_utils.setup_bidirectional_hydra("n", "warning", "[w", "]w", function()
+					vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity["WARN"] })
+				end, function()
+					vim.diagnostic.goto_next({ severity = vim.diagnostic.severity["WARN"] })
+				end, opts)
+			end,
+		})
 
 		vim.diagnostic.config({
 			signs = {
@@ -138,14 +161,15 @@ return {
 
 		local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-		-- configure bashls server
-		lspconfig["bashls"].setup({
+		vim.lsp.config("*", {
 			capabilities = capabilities,
-			on_attach = on_attach,
 		})
 
+		-- configure bashls server
+		vim.lsp.config("bashls", {})
+
 		-- configure clangd server
-		lspconfig["clangd"].setup({
+		vim.lsp.config("clangd", {
 			cmd = {
 				"clangd",
 				"--background-index",
@@ -154,28 +178,41 @@ return {
 				"--completion-style=detailed",
 				"--function-arg-placeholders",
 				"--fallback-style=llvm",
-				"--compile-commands-dir=" .. vim.fn.getcwd(),
-				-- "--compile-commands-dir=.",
+				-- "--compile-commands-dir=" .. vim.fn.getcwd(),
+				"--compile-commands-dir=.",
 			},
 			init_options = {
 				usePlaceholders = true,
 				completeUnimported = true,
 				clangdFileStatus = true,
 			},
-			capabilities = capabilities,
 			on_attach = function(client, bufnr)
-				on_attach(client, bufnr)
+				vim.api.nvim_buf_create_user_command(bufnr, "ClangdSwitchSourceHeader", function()
+					switch_source_header(client, bufnr)
+				end, {
+					bar = true,
+					desc = "Switch Source/Header (C/C++)",
+				})
 				local clang_opts = { noremap = true, silent = true }
 				clang_opts.buffer = bufnr
 				clang_opts.desc = "Switch Source/Header (C/C++)"
 				vim.keymap.set("n", "gs", "<cmd>ClangdSwitchSourceHeader<cr>", clang_opts)
+				vim.api.nvim_create_autocmd("LspDetach", {
+					group = vim.api.nvim_create_augroup("conf_lsp_attach_detach", { clear = false }),
+					buffer = bufnr,
+					callback = function(args)
+						if args.data.client_id == client.id then
+							vim.keymap.del("n", "gs", { buffer = bufnr })
+							vim.api.nvim_buf_del_user_command(bufnr, "ClangdSwitchSourceHeader")
+							return true -- Delete this autocmd.
+						end
+					end,
+				})
 			end,
 		})
 
 		-- configure lua server (with special settings)
-		lspconfig["lua_ls"].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
+		vim.lsp.config("lua_ls", {
 			settings = { -- custom settings for lua
 				Lua = {
 					-- make the language server recognize "vim" global
@@ -193,9 +230,7 @@ return {
 			},
 		})
 
-		lspconfig["qmlls"].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
+		vim.lsp.config("qmlls", {
 			cmd = { vim.env.QT_BIN_DIR ~= nil and vim.fs.joinpath(vim.env.QT_BIN_DIR, "qmlls") or "qmlls" },
 			filetypes = { "qmljs", "qml" },
 			handlers = {
@@ -214,9 +249,7 @@ return {
 			},
 		})
 
-		lspconfig["fish_lsp"].setup({
-			capabilities = capabilities,
-			on_attach = on_attach,
+		vim.lsp.config("fish_lsp", {
 			cmd = { "fish-lsp", "start" },
 			filetypes = { "fish" },
 			cmd_env = { fish_lsp_show_client_popups = false },
