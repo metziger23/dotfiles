1423403626c378f572586114e5f838d711c16f3e
Гулый Михаил Владимирович <metziger23@gmail.com>
2025-10-17 21:35:40 +0300

    use Fira Code 11 font for awesome wm

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -79,6 +79,7 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.font = "Fira Code 11"
 
 -- This is used later as the default terminal and editor to run.
 terminal = "kitty"
