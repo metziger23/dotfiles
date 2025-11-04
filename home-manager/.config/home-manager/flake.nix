ee7323920daccfb724c721498c8af17c88d0f5fa
Гулый Михаил Владимирович <metziger23@gmail.com>
2025-11-04 15:48:46 +0300

    added home-manager

---
+++ home-manager/.config/home-manager/flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Home Manager configuration of mikhail";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixGL = {
+      url = "github:nix-community/nixGL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    }; 
+  };
+
+outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    nixGL,
+    ...
+  }: {
+    # Replace with your own username
+    homeConfigurations."mikhail" = home-manager.lib.homeManagerConfiguration {
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config = {
+          allowUnfree = true;
+          allowUnfreePredicate = _: true;
+        };
+      };
+      extraSpecialArgs = {
+        inherit nixGL;
+      };
+      modules = [./home.nix];
+    };
+  };
+}
