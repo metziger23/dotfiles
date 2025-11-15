941d69b3cf5a560d655f81408d7dfeac7a191407
metziger23 <metziger23@gmail.com>
2025-11-15 14:51:50 +0300

    modified debugprint

--- nvim/.config/nvim/lua/plugins/debugprint.lua
+++ nvim/.config/nvim/lua/plugins/debugprint.lua
@@ -1,23 +1,51 @@
--- stylua: ignore start
+ -- stylua: ignore start
 local colors = {
-	"red", "green", "blue", "orange", "purple",
-	"teal", "pink", "lime", "white", "navy", "olive",
-	 "silver", "gray", "gold", "cyan", "magenta",
-  "brown", "darkgreen", "skyblue", "indigo",
-  "coral", "yellow", "violet", "firebrick",
-	 "royalblue", "sienna", "tomato", "slateblue",
+"red", "green", "blue", "orange", "purple", "teal", "pink", "lime",
+"white", "navy", "olive", "silver", "gray", "gold", "cyan", "magenta",
+"brown", "darkgreen", "skyblue", "indigo", "coral", "yellow", "violet", "firebrick",
+"royalblue", "sienna", "tomato", "slateblue",
+"lightgrey", "lavenderblush", "deeppink", "seashell",
+"lightsalmon", "lightgreen", "deepskyblue", "mistyrose",
+"dimgray", "navajowhite", "peru", "darkgrey",
+"steelblue", "orangered", "mediumslateblue", "blueviolet",
+"cornflowerblue", "beige", "goldenrod", "rosybrown",
+"darkblue", "aliceblue", "mediumblue", "dodgerblue",
+"limegreen", "lightsteelblue", "lightslategray", "seagreen",
+"mediumvioletred", "slategrey", "darkslategrey", "turquoise",
+"paleturquoise", "lightgoldenrodyellow", "darkseagreen", "lightcyan",
+"lightcoral", "mediumseagreen", "palegoldenrod", "palegreen",
+"darkslateblue", "moccasin", "forestgreen", "darkkhaki",
+"chartreuse", "floralwhite", "snow", "fuchsia",
+"orchid", "darkorchid", "darkred", "darksalmon",
+"crimson", "palevioletred", "lightseagreen", "ivory",
+"powderblue", "aquamarine", "darkturquoise", "lavender",
+"azure", "mediumturquoise", "lightgray", "transparent",
+"gainsboro", "olivedrab", "papayawhip", "midnightblue",
+"yellowgreen", "slategray", "grey", "wheat",
+"darkgoldenrod", "lawngreen", "lightslategrey", "burlywood",
+"aqua", "saddlebrown", "oldlace", "lightskyblue",
+"dimgrey", "darkorange", "lightblue", "khaki",
+"mediumpurple", "linen", "mediumorchid", "indianred",
+"maroon", "darkgray", "hotpink", "cadetblue",
+"darkslategray", "plum", "mediumspringgreen", "thistle",
+"mintcream", "darkmagenta", "lemonchiffon", "bisque",
+"antiquewhite", "whitesmoke", "lightpink", "darkcyan",
+"tan", "blanchedalmond", "honeydew", "salmon",
+"lightyellow", "springgreen", "cornsilk", "sandybrown",
+"mediumaquamarine", "darkviolet", "darkolivegreen", "peachpuff",
+"greenyellow", "ghostwhite", "chocolate",
 }
 -- stylua: ignore end
 
 local js_like = {
-	left = 'console.info("',
+	left = 'console.log("',
 	right = '")',
 	mid_var = '", ',
 	right_var = ")",
 }
 
 local qt_like = {
-	left = 'qInfo() << "',
+	left = 'qDebug() << "',
 	right = '";',
 	mid_var = '" << ',
 	right_var = ";",
@@ -47,7 +75,7 @@ return {
 			local default_result_number = tonumber(default_result:match("%[(%d+)%]"))
 			---@diagnostic disable-next-line: param-type-mismatch
 			local color_number = math.fmod(default_result_number, #colors) + 1
-			return colors[color_number]
+			return "N1ZpIq " .. colors[color_number]
 		end
 
 		require("debugprint").setup({
