f71418e171e12e78c248cd0c673a1fbae4048ac4
metziger23 <metziger23@gmail.com>
2025-11-15 14:55:30 +0300

    dropbar only show path

--- nvim/.config/nvim/lua/plugins/dropbar.lua
+++ nvim/.config/nvim/lua/plugins/dropbar.lua
@@ -5,6 +5,35 @@ return {
 		"nvim-tree/nvim-web-devicons", -- optional dependency
 	},
 	opts = {
+		bar = {
+			sources = function(buf, _)
+				local sources = require("dropbar.sources")
+				if vim.bo[buf].buftype == "terminal" then
+					return {
+						sources.terminal,
+					}
+				end
+				return { sources.path }
+				-- if vim.bo[buf].ft == 'markdown' then
+				--   return {
+				--     sources.path,
+				--     sources.markdown,
+				--   }
+				-- end
+				-- if vim.bo[buf].buftype == 'terminal' then
+				--   return {
+				--     sources.terminal,
+				--   }
+				-- end
+				-- return {
+				--   sources.path,
+				--   utils.source.fallback({
+				--     sources.lsp,
+				--     sources.treesitter,
+				--   }),
+				-- }
+			end,
+		},
 		menu = {
 			win_configs = {
 				border = "rounded",
