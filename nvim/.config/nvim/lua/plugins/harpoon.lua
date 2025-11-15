0ba8f0aa2a1e0140f61a616881a418a6f8338e4b
metziger23 <metziger23@gmail.com>
2025-11-15 14:52:46 +0300

    enable harpoon for now

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -1,6 +1,16 @@
+local normalize_list = function(t)
+	local normalized = {}
+	for _, v in pairs(t) do
+		if v ~= nil then
+			table.insert(normalized, v)
+		end
+	end
+	return normalized
+end
+
 return {
 	"theprimeagen/harpoon",
-	enabled = false,
+	-- enabled = false,
 	branch = "harpoon2",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
@@ -8,17 +18,44 @@ return {
 	end,
 	keys = {
       -- stylua: ignore start
+    { "<C-c>", "<cmd>close<cr>", ft = "harpoon" , desc = "Close harpoon" },
     { "<leader>A", function() require("harpoon"):list():add() end, desc = "Harpoon Add File", },
     { "<leader>a", function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list(), { border = "rounded" }) end, desc = "Harpoon Quick Menu", },
-    { "<leader>1", function() require("harpoon"):list():select(1) end, desc = "which_key_ignore", },
-    { "<leader>2", function() require("harpoon"):list():select(2) end, desc = "which_key_ignore", },
-    { "<leader>3", function() require("harpoon"):list():select(3) end, desc = "which_key_ignore", },
-    { "<leader>4", function() require("harpoon"):list():select(4) end, desc = "which_key_ignore", },
-    { "<leader>5", function() require("harpoon"):list():select(5) end, desc = "which_key_ignore", },
-    { "<leader>6", function() require("harpoon"):list():select(6) end, desc = "which_key_ignore", },
-    { "<leader>7", function() require("harpoon"):list():select(7) end, desc = "which_key_ignore", },
-    { "<leader>8", function() require("harpoon"):list():select(8) end, desc = "which_key_ignore", },
-    { "<leader>9", function() require("harpoon"):list():select(9) end, desc = "which_key_ignore", },
+    { "<BS>t", function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list(), { border = "rounded" }) end, desc = "Harpoon Quick Menu", },
 		-- stylua: ignore end
+
+		{
+			"<leader>n",
+			function()
+				local harpoon = require("harpoon")
+				require("snacks").picker({
+					finder = function()
+						local file_paths = {}
+						local list = normalize_list(harpoon:list().items)
+						for _, item in ipairs(list) do
+							table.insert(file_paths, { text = item.value, file = item.value })
+						end
+						return file_paths
+					end,
+					win = {
+						input = {
+							keys = { ["dd"] = { "harpoon_delete", mode = { "n", "x" } } },
+						},
+						list = {
+							keys = { ["dd"] = { "harpoon_delete", mode = { "n", "x" } } },
+						},
+					},
+					actions = {
+						harpoon_delete = function(picker, item)
+							local to_remove = item or picker:selected()
+							harpoon:list():remove({ value = to_remove.text })
+							harpoon:list().items = normalize_list(harpoon:list().items)
+							picker:find({ refresh = true })
+						end,
+					},
+				})
+			end,
+			desc = "which_key_ignore",
+		},
 	},
 }
