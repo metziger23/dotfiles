b8918888d7a67db34d46f257fb215937b90d6b3c
Гулый Михаил Владимирович <m.gulyy@2gis.ru>
2025-12-07 14:17:37 +0300

    moved from satellite to dstein64/nvim-scrollview

---
+++ nvim/.config/nvim/lua/plugins/nvim-scrollview.lua
@@ -0,0 +1,12 @@
+return {
+	"dstein64/nvim-scrollview",
+	event = { "BufReadPre", "BufNewFile" },
+	after = "gitsigns.nvim",
+	config = function()
+		require("scrollview").setup({
+			-- current_only = true,
+			signs_on_startup = { "diagnostics", "folds", "search" },
+		})
+		require("scrollview.contrib.gitsigns").setup()
+	end,
+}
--- nvim/.config/nvim/lua/plugins/satellite.lua
+++ nvim/.config/nvim/lua/plugins/satellite.lua
@@ -1,5 +1,9 @@
 return {
 	"lewis6991/satellite.nvim",
+	-- NOTE: moved to dstein64/nvim-scrollview because of
+	-- https://github.com/lewis6991/satellite.nvim/issues/90
+	-- (reproduced using "gf" with diffview)
+	enabled = false,
 	lazy = false,
 	opts = {},
 }
