3f91bc86db025d77e1616d314b4e6464d1b685cc
Гулый Михаил Владимирович <m.gulyy@2Gis.ru>
2026-01-11 22:01:49 +0300

    pinned treesitter to master branch

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
+	branch = "master",
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
 	config = function()
