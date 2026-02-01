976a48392b404767046477f67cba7cfe344c5181
metziger23 <metziger23@gmail.com>
2026-02-01 14:26:08 +0300

    returned stay-centered

--- nvim/.config/nvim/lua/plugins/stay-centered.lua
+++ nvim/.config/nvim/lua/plugins/stay-centered.lua
@@ -1,11 +1,13 @@
 return {
 	"arnamak/stay-centered.nvim",
 	lazy = false,
-	enabled = false,
+	-- enabled = false,
 	config = function()
 		require("stay-centered").setup({
 			-- Add any configurations here, like skip_filetypes if needed
 			skip_filetypes = { "kitty-scrollback" },
+			-- fix for https://github.com/arnamak/stay-centered.nvim/issues/23
+			allow_scroll_move = false,
 		})
 	end,
 }
