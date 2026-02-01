9a636c34965ee115d0677d9cf8a7ecaa5330585c
metziger23 <metziger23@gmail.com>
2026-02-01 14:26:08 +0300

    disabled snacks-indent.lua because it gives flickering

--- nvim/.config/nvim/lua/plugins/snacks-indent.lua
+++ nvim/.config/nvim/lua/plugins/snacks-indent.lua
@@ -2,6 +2,8 @@ return {
 	"folke/snacks.nvim",
 	opts = {
 		indent = {
+			-- NOTE: disabled cause this plugins causes flickering when doing <C-l>
+			enabled = false,
 			scope = {
 				enabled = false,
 			},
