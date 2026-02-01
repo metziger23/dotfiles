3207208426c04ec66e68887ae92783cea3c9d771
metziger23 <metziger23@gmail.com>
2026-02-01 14:25:03 +0300

    added toggling and removing global marks

--- nvim/.config/nvim/lua/plugins/marks.lua
+++ nvim/.config/nvim/lua/plugins/marks.lua
@@ -1,3 +1,70 @@
+local function for_each_global_mark(callback)
+	local marks = vim.fn.getmarklist()
+
+	for _, mark_info in ipairs(marks) do
+		if mark_info.mark:match("'[A-Z]") then
+			local char = mark_info.mark:sub(2)
+			if callback then
+				callback(char, mark_info)
+			end
+		end
+	end
+end
+
+local function is_global_mark_set(mark)
+	local marks = vim.api.nvim_exec("marks", true)
+	return string.find(marks, "%s" .. mark .. " ")
+end
+
+local function next_available_global_mark()
+	for ascii = 65, 90 do -- ASCII values for A-Z
+		local mark = string.char(ascii)
+		if not is_global_mark_set(mark) then
+			return mark
+		end
+	end
+	return nil
+end
+
+local function remove_global_mark_if_exists()
+	local bufnr = vim.api.nvim_get_current_buf()
+	local row, _ = unpack(vim.api.nvim_win_get_cursor(0))
+	local mark_removed = false
+
+	for_each_global_mark(function(char, info)
+		if info.pos[1] == bufnr and info.pos[2] == row then
+			vim.cmd("delmarks " .. char)
+			mark_removed = true
+		end
+	end)
+
+	return mark_removed
+end
+
+local function set_global_mark()
+	if vim.api.nvim_buf_get_name(0) == "" then
+		print("Cannot set a global mark in an unsaved buffer")
+		return
+	end
+
+	local mark = next_available_global_mark()
+	if mark then
+		vim.cmd("mark " .. mark)
+	end
+end
+
+local function toggle_global_mark()
+	if not remove_global_mark_if_exists() then
+		-- NOTE: to remove all marks on the current line
+		require("marks").delete_line()
+		set_global_mark()
+	end
+end
+
+local function clear_all_global_marks()
+	vim.cmd("delmarks A-Z")
+end
+
 local function preview_mark()
 	local a = vim.api
 	a.nvim_echo({ { "press letter mark to preview, or press <esc> to quit" } }, true, {})
@@ -45,24 +112,40 @@ return {
 		local marks = require("marks")
 		marks.preview = preview_mark
 		marks.setup({
+			-- TODO: rewrite it via vim.keymap.set to add desc
 			mappings = {
-				prev = false,
+				set = "m",
+				set_next = "m,",
+				toggle = "m;",
 				next = false,
-				prev_bookmark = false,
+				prev = false,
+				preview = "m<leader>",
 				next_bookmark = false,
+				prev_bookmark = false,
+				delete = "dm",
+				delete_line = "dm-",
+				delete_bookmark = "dm=",
+				delete_buf = "dm<space>", -- NOTE: deletes only local marks
 			},
 		})
 		local hydra_utils = require("../utils/hydra_utils")
-		hydra_utils.setup_bidirectional_hydra("n", "mark", "m[", "m]", function()
+		hydra_utils.setup_bidirectional_hydra("n", "mark", "[<BS>", "]<BS>", function()
 			marks.prev()
 		end, function()
 			marks.next()
 		end)
 
-		hydra_utils.setup_bidirectional_hydra("n", "bookmark", "m{", "m}", function()
+		hydra_utils.setup_bidirectional_hydra("n", "bookmark", "[<Del>", "]<Del>", function()
 			marks.prev_bookmark()
 		end, function()
 			marks.next_bookmark()
 		end)
+
+		local opts = { noremap = true }
+		opts.desc = "Toggle global mark"
+		vim.keymap.set("n", "m:", toggle_global_mark, opts)
+
+		opts.desc = "Clear all global marks"
+		vim.keymap.set("n", "dm<BS>", clear_all_global_marks, opts)
 	end,
 }
