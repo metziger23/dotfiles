4784ca22d1933c2141affc43b3afb8a87240324c
metziger23 <metziger23@gmail.com>
2026-02-01 14:26:23 +0300

    added TodoFzfLua

--- nvim/.config/nvim/lua/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -1,29 +1,15 @@
 return {
 	"folke/todo-comments.nvim",
-	cmd = { "TodoTrouble" },
+	cmd = { "TodoTrouble", "TodoFzfLua" },
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = { "nvim-lua/plenary.nvim", "folke/snacks.nvim" },
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
+	opts = {},
+	keys = {
+		{
+			"<BS>T",
+			"<Cmd>TodoFzfLua<Cr>",
+			desc = "Fzf todo comments",
+			mode = { "n", "x" },
+		},
 	},
-	-- keys = {
-	-- 	{
-	-- 		"<BS>t",
-	-- 		function()
-	-- 			require("snacks").picker.todo_comments()
-	-- 		end,
-	-- 		desc = "Todo",
-	-- 		mode = { "n", "x" },
-	-- 	},
-	-- 	{
-	-- 		"<BS>T",
-	-- 		function()
-	-- 			require("snacks").picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } })
-	-- 		end,
-	-- 		desc = "Todo/Fix/Fixme",
-	-- 		mode = { "n", "x" },
-	-- 	},
-	-- },
 }
