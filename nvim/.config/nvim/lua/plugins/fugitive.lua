ac4f87ff7c73d37c35f9dea56deef2498a6fd7ce
metziger23 <metziger23@gmail.com>
2026-03-14 12:58:31 +0300

    added vim-fugitive

---
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -0,0 +1,22 @@
+return {
+	{
+		"tpope/vim-fugitive",
+    enabled = false,
+		config = function()
+			-- Optional: Add your keymaps here
+			vim.keymap.set("n", "<leader>f", "<cmd>Git<CR>", { desc = "Git status" })
+			-- ... other keymaps
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "fugitive",
+				callback = function()
+					vim.opt_local.bufhidden = ""
+				end,
+			})
+
+			vim.cmd([[
+      let g:fugitive_focus_gained = v:false
+      ]])
+		end,
+	},
+}
