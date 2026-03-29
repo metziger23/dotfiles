e53150033628d9dcd7373d838609a3b72d45ab1e
metziger23 <metziger23@gmail.com>
2026-03-29 12:54:34 +0300

    Revert "show macro recording on the left for mini-statusline"
    This reverts commit 53a77e83f5c5920827828036f5af13645e83fea0.

--- nvim/.config/nvim/lua/plugins/mini-statusline.lua
+++ nvim/.config/nvim/lua/plugins/mini-statusline.lua
@@ -87,13 +87,12 @@ return {
 						get_overseer_task_status("SUCCESS"),
 						get_overseer_task_status("RUNNING"),
 						{ hl = "MiniStatuslineFilename", strings = { get_relative_filename() } },
+						"%=", -- End left alignment
 						get_noice_status(
 							require("noice").api.status.mode.get,
 							require("noice").api.status.mode.has,
 							mode_hl
 						),
-						{ hl = "MiniStatuslineInactive", strings = { "" } }, -- NOTE: spacer
-						"%=", -- End left alignment
 						get_noice_status(
 							require("noice").api.status.search.get,
 							require("noice").api.status.search.has,
@@ -101,12 +100,7 @@ return {
 						),
 						-- { hl = "MiniStatuslineFilename", strings = { get_cwd() } },
 						{ hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
-						{
-							hl = mode_hl,
-							strings = { --[[ search, ]]
-								location,
-							},
-						},
+						{ hl = mode_hl, strings = { --[[ search, ]] location } },
 					})
 				end,
 			},
