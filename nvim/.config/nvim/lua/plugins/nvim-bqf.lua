9a2dd2753552f2d30f7a70a7aeafd7f0c91c0b6e
metziger23 <metziger23@gmail.com>
2026-04-26 21:37:05 +0300

    fugitive integration for nvim-bqf

--- nvim/.config/nvim/lua/plugins/nvim-bqf.lua
+++ nvim/.config/nvim/lua/plugins/nvim-bqf.lua
@@ -1,1 +1,19 @@
-return { "kevinhwang91/nvim-bqf", dependencies = "junegunn/fzf" }
+return {
+	"kevinhwang91/nvim-bqf",
+	dependencies = "junegunn/fzf",
+	config = function()
+		require("bqf").setup({
+			preview = {
+				should_preview_cb = function(bufnr)
+					local bufname = vim.api.nvim_buf_get_name(bufnr)
+					if bufname:match("^fugitive://") then
+						vim.api.nvim_buf_call(bufnr, function()
+							vim.cmd(("do fugitive BufReadCmd %s"):format(bufname))
+						end)
+					end
+					return true
+				end,
+			},
+		})
+	end,
+}
