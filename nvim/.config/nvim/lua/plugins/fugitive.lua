259c287c376839dfb38ba3ffb5e7f2c4430baf23
metziger23 <metziger23@gmail.com>
2026-04-28 23:45:45 +0300

    enabled fugitive

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -1,22 +1,21 @@
 return {
-	{
-		"tpope/vim-fugitive",
-    enabled = false,
-		config = function()
-			-- Optional: Add your keymaps here
-			vim.keymap.set("n", "<leader>f", "<cmd>Git<CR>", { desc = "Git status" })
-			-- ... other keymaps
 
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = "fugitive",
-				callback = function()
-					vim.opt_local.bufhidden = ""
-				end,
-			})
+	"tpope/vim-fugitive",
+	-- enabled = false,
+	config = function()
+		-- Optional: Add your keymaps here
+		vim.keymap.set("n", "<M-g>", "<cmd>Git<CR>", { desc = "Git status" })
+		-- ... other keymaps
 
-			vim.cmd([[
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "fugitive",
+			callback = function()
+				vim.opt_local.bufhidden = ""
+			end,
+		})
+
+		vim.cmd([[
       let g:fugitive_focus_gained = v:false
       ]])
-		end,
-	},
+	end,
 }
