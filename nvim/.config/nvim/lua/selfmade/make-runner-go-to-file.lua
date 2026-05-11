774f1a4b3d38f1b181bb5426db14e0ff8eb9bca7
metziger23 <metziger23@gmail.com>
2026-05-11 20:26:13 +0300

    support multiline text for make-runner-go-to-file

--- nvim/.config/nvim/lua/selfmade/make-runner-go-to-file.lua
+++ nvim/.config/nvim/lua/selfmade/make-runner-go-to-file.lua
@@ -1,13 +1,93 @@
 local M = {}
 
+local function get_start_end_indices(text)
+	local col = vim.api.nvim_win_get_cursor(0)[2]
+	local line = vim.api.nvim_get_current_line()
+	local match_number = 1
+	local start_idx = nil
+	local end_idx = nil
+	while true do
+		start_idx, end_idx = line:find(text, match_number, true)
+		start_idx = start_idx - 1
+		end_idx = end_idx - 1
+		if start_idx <= col and col <= end_idx then
+			break
+		end
+
+		if not start_idx or not end_idx then
+			break
+		end
+
+		match_number = match_number + 1
+	end
+	return start_idx, end_idx
+end
+
+local function get_buffer_viewport_width()
+	local winid = vim.fn.win_getid()
+	local info = vim.fn.getwininfo(winid)[1]
+	local text_width = info.width - info.textoff
+	return text_width
+end
+
+local function get_prev_line_text(row)
+	if row <= 1 then
+		return ""
+	end
+
+	local prev_line = vim.api.nvim_buf_get_lines(0, row - 2, row - 1, false)[1]
+	if #prev_line < get_buffer_viewport_width() then
+		return ""
+	end
+
+	local text = prev_line:match("^.*%s+(.+)$")
+	if text then
+		return text
+	else
+		return get_prev_line_text(row - 1) .. prev_line
+	end
+end
+
+local function get_next_line_text(row)
+	if row >= vim.api.nvim_buf_line_count(0) then
+		return ""
+	end
+
+	local next_line = vim.api.nvim_buf_get_lines(0, row, row + 1, false)[1]
+	local text = next_line:match("^(.-)%s")
+	if text then
+		return text
+	else
+		if #next_line == get_buffer_viewport_width() then
+			return next_line .. get_next_line_text(row + 1)
+		else
+			return next_line
+		end
+	end
+end
+
+local function get_multiline_text(text)
+	local start_idx, end_idx = get_start_end_indices(text)
+	local row = vim.api.nvim_win_get_cursor(0)[1] -- 1-based
+	if start_idx == 0 then
+		text = get_prev_line_text(row) .. text
+	end
+
+	if end_idx + 1 >= get_buffer_viewport_width() then
+		text = text .. get_next_line_text(row)
+	end
+
+	return text
+end
+
 local function get_path_and_position()
-	local cWORD = vim.fn.expand("<cWORD>")
-	local path, lnum, cnum = cWORD:match("^(.-):(%d+):(%d+)")
+	local text = get_multiline_text(vim.fn.expand("<cWORD>"))
+	local path, lnum, cnum = text:match("^(.-):(%d+):(%d+)")
 	if path and lnum and cnum then
 		return path, lnum, cnum
 	end
 
-	path, lnum = cWORD:match("^(.+):(%d+)")
+	path, lnum = text:match("^(.+):(%d+)")
 	if path and lnum then
 		return path, lnum, nil
 	end
@@ -21,13 +101,11 @@ local function strip_file_prefix(path)
 end
 
 function M.go_to_file()
-	-- TODO: go to my mega tag N1ZpIq
+	-- TODO: go to my mega tag
 
 	-- TODO: don't open if another window is of filetype make-runner
 
 	-- TODO: setup previous window selector like in snacks picker
-
-	-- TODO: support multiline paths
 
 	local path, lnum, cnum = get_path_and_position()
 	path = strip_file_prefix(path)
