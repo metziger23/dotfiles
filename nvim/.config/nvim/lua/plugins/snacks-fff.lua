abba406c8f8ec97c03e0f93782b9bc20501c0800
metziger23 <metziger23@gmail.com>
2026-06-16 00:10:01 +0300

    use my snacks-fff for grep and files picker

---
+++ nvim/.config/nvim/lua/plugins/snacks-fff.lua
@@ -0,0 +1,27 @@
+return {
+	"metziger23/snacks-fff.nvim",
+	dependencies = {
+		"folke/snacks.nvim",
+		{
+			"dmtrKovalenko/fff.nvim",
+			build = function()
+				require("fff.download").download_or_build_binary()
+			end,
+		},
+	},
+  config = function ()
+    require("snacks-fff").setup()
+
+    vim.keymap.set("n", "<BS>f", function()
+      require("snacks-fff").find_files()
+    end, { desc = "Smart" })
+
+    vim.keymap.set("n", "<BS><tab>", function()
+      require("snacks-fff").find_files()
+    end, { desc = "Smart" })
+
+    vim.keymap.set("n", "<BS><leader>", function()
+      require("snacks-fff").live_grep({ grep_modes = { "regex", "fuzzy", "plain" } })
+    end, { desc = "Live grep" })
+  end
+}
