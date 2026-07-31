5f03fcec600ab07fd3dfab9e107358cfaf538062
metziger23 <metziger23@gmail.com>
2026-07-31 19:51:54 +0300

    added join_work_under_the_cursor_cpp and join_work_under_the_cursor_qml

--- nvim/lua/plugins/debugprint.lua
+++ nvim/lua/plugins/debugprint.lua
@@ -12,6 +12,64 @@ local qt_like = {
 	right_var = ";",
 }
 
+local function join_work_under_the_cursor_qml(name, buf)
+	local lnum = vim.api.nvim_win_get_cursor(0)[1] -- 1-based
+	if lnum <= 1 then
+		return
+	end
+
+	if not name or name == "" then
+		return
+	end
+
+	local prev_lnum0 = lnum - 2
+	local cur_lnum0 = lnum - 1
+
+	local prev = vim.api.nvim_buf_get_lines(buf, prev_lnum0, prev_lnum0 + 1, false)[1] or ""
+	local indent = prev:match("^%s*") or ""
+
+	-- убрать ";" в конце строки (с пробелами), затем добавить " <<"
+	local new_prev = prev
+		:gsub("%s*;%s*$", "") -- remove trailing semicolon
+    :gsub("%s*%)%s*$", "") -- remove trailing paren
+		:gsub("%s*$", "") -- trim end
+		.. ","
+
+  local safe_name = name:gsub("%%", "%%%%")
+  local new_cur = indent .. string.format([["%s", %s);]], safe_name, safe_name)
+
+	vim.api.nvim_buf_set_lines(buf, prev_lnum0, prev_lnum0 + 1, false, { new_prev })
+	vim.api.nvim_buf_set_lines(buf, cur_lnum0, cur_lnum0 + 1, false, { new_cur })
+end
+
+local function join_work_under_the_cursor_cpp(name, buf)
+	local lnum = vim.api.nvim_win_get_cursor(0)[1] -- 1-based
+	if lnum <= 1 then
+		return
+	end
+
+	if not name or name == "" then
+		return
+	end
+
+	local prev_lnum0 = lnum - 2
+	local cur_lnum0 = lnum - 1
+
+	local prev = vim.api.nvim_buf_get_lines(buf, prev_lnum0, prev_lnum0 + 1, false)[1] or ""
+	local indent = prev:match("^%s*") or ""
+
+	-- убрать ";" в конце строки (с пробелами), затем добавить " <<"
+	local new_prev = prev
+		:gsub("%s*;%s*$", "") -- remove trailing semicolon
+		:gsub("%s*$", "") -- trim end
+		.. " <<"
+
+	local new_cur = indent .. string.format([["%s" << %s;]], name, name)
+
+	vim.api.nvim_buf_set_lines(buf, prev_lnum0, prev_lnum0 + 1, false, { new_prev })
+	vim.api.nvim_buf_set_lines(buf, cur_lnum0, cur_lnum0 + 1, false, { new_cur })
+end
+
 return {
 	"andrewferrier/debugprint.nvim", -- opts = {},
 	enabled = true,
@@ -81,5 +139,39 @@ return {
 		})
 
 		vim.fn.setreg("n", tag.search_helper_tag)
+		local group = vim.api.nvim_create_augroup("CppFtMaps", { clear = true })
+
+		vim.api.nvim_create_autocmd("FileType", {
+			group = group,
+			pattern = { "cpp", "c", "objc", "objcpp" },
+			callback = function(ev)
+				local buf = ev.buf
+
+				vim.keymap.set("n", "<leader>j", function()
+					join_work_under_the_cursor_cpp(vim.fn.expand("<cword>"), buf)
+				end, { buffer = buf, silent = true, desc = "qDebug: join cword" })
+
+				vim.keymap.set("n", "<leader>J", function()
+					join_work_under_the_cursor_cpp(vim.fn.expand("<cWORD>"), buf)
+				end, { buffer = buf, silent = true, desc = "qDebug: join cWORD" })
+			end,
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			group = group,
+			pattern = { "qml" },
+			callback = function(ev)
+				local buf = ev.buf
+
+				vim.keymap.set("n", "<leader>j", function()
+					join_work_under_the_cursor_qml(vim.fn.expand("<cword>"), buf)
+				end, { buffer = buf, silent = true, desc = "qDebug: join cword" })
+
+				vim.keymap.set("n", "<leader>J", function()
+					join_work_under_the_cursor_qml(vim.fn.expand("<cWORD>"), buf)
+				end, { buffer = buf, silent = true, desc = "qDebug: join cWORD" })
+			end,
+		})
+
 	end,
 }
