754dff12aec940659a32328612ceced061244da9
metziger23 <metziger23@gmail.com>
2026-08-03 00:16:21 +0300

    added JustSelectTaskToRun and removed picker.lua

--- nvim/lua/selfmade/just-runner/init.lua
+++ nvim/lua/selfmade/just-runner/init.lua
@@ -12,3 +12,26 @@ end, {
 		return require("selfmade.just-runner.recency-sorter").get_sorted_tasks()
 	end,
 })
+
+vim.api.nvim_create_user_command("JustSelectTaskToRun", function()
+	local tasks = require("selfmade.just-runner.recency-sorter").get_sorted_tasks()
+	if tasks == nil or #tasks == 0 then
+		vim.notify("No tasks", vim.log.levels.WARN)
+		return
+	end
+
+	vim.ui.select(tasks, {
+		prompt = "Select task to execute:",
+		format_item = function(item)
+			return item
+		end,
+	}, function(choice)
+		if choice then
+			require("selfmade.just-runner.actions").run_task(choice, vim.fn.getcwd())
+		end
+	end)
+end, {
+	desc = "Select task from justfile using vim.ui.select",
+})
+
+vim.keymap.set("n", "<leader>,", "<cmd>JustSelectTaskToRun<CR>", { desc = "just: select task to run" })
